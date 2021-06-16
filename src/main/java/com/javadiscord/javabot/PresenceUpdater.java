e7ba22987461b1bb4d1e87b5b247ffeeb5edd239
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-06-16 21:17:11 +0200

    Updated to check for empty list.

--- src/main/java/com/javadiscord/javabot/PresenceUpdater.java
+++ src/main/java/com/javadiscord/javabot/PresenceUpdater.java
@@ -85,7 +85,9 @@ public class PresenceUpdater extends ListenerAdapter {
                 this.jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
             }
             if (currentActivityIndex >= this.activities.size()) currentActivityIndex = 0;
-            this.jda.getPresence().setActivity(this.activities.get(currentActivityIndex++).apply(this.jda));
+            if (this.activities.size() > 0) {
+                this.jda.getPresence().setActivity(this.activities.get(currentActivityIndex++).apply(this.jda));
+            }
         }, 0, this.delay, this.delayUnit);
     }
 
