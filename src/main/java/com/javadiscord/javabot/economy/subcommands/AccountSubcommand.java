d6f9dde48572d15bbd14437b445a41e9aa6ecd42
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-07-30 15:48:58 +0200

    Fix empty transactions on account info.

--- src/main/java/com/javadiscord/javabot/economy/subcommands/AccountSubcommand.java
+++ src/main/java/com/javadiscord/javabot/economy/subcommands/AccountSubcommand.java
@@ -4,6 +4,7 @@ import com.javadiscord.javabot.Bot;
 import com.javadiscord.javabot.commands.SlashCommandHandler;
 import com.javadiscord.javabot.economy.EconomyService;
 import com.javadiscord.javabot.economy.model.Account;
+import com.javadiscord.javabot.economy.model.Transaction;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ISnowflake;
 import net.dv8tion.jda.api.entities.User;
@@ -16,6 +17,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class AccountSubcommand implements SlashCommandHandler {
@@ -28,46 +32,61 @@ public class AccountSubcommand implements SlashCommandHandler {
 			var service = new EconomyService(Bot.dataSource);
 			Account account = service.getOrCreateAccount(event.getUser().getIdLong());
 			var transactions = service.getRecentTransactions(account.getUserId(), 5);
-			List<RestAction<User>> userRestActions = new ArrayList<>();
-			for (var t : transactions) {
-				if (t.getFromUserId() != null) {
-					userRestActions.add(event.getJDA().retrieveUserById(t.getFromUserId()));
-				}
-				if (t.getToUserId() != null) {
-					userRestActions.add(event.getJDA().retrieveUserById(t.getToUserId()));
-				}
-			}
-			RestAction.allOf(userRestActions).queue(users -> {
-				Map<Long, User> usersMap = users.stream().distinct().collect(Collectors.toMap(ISnowflake::getIdLong, user -> user));
-				String transactionsText = transactions.stream()
-						.map(t -> {
-							String value = String.format("%,d", t.getValue());
-							String otherUserString;
-							if (t.getFromUserId() != null && t.getFromUserId().equals(account.getUserId())) {
-								value = "- " + value;
-								otherUserString = (usersMap.get(t.getToUserId()) == null ? "System" : usersMap.get(t.getToUserId()).getAsTag());
-							} else {
-								value = "+ " + value;
-								otherUserString = (usersMap.get(t.getFromUserId()) == null ? "System" : usersMap.get(t.getFromUserId()).getAsTag());
-							}
-							value = " ".repeat(VALUE_BUFFER_SPACE - value.length()) + value;
-							otherUserString = " ".repeat(VALUE_BUFFER_SPACE - otherUserString.length()) + otherUserString;
-							return String.format("%s\n%s\n%s", t.getCreatedAt().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss 'UTC'")), value, otherUserString);
-						})
-						.collect(Collectors.joining("\n"));
-				EmbedBuilder embedBuilder = new EmbedBuilder()
-						.setTitle("Account Information for " + event.getUser().getAsTag())
-						.setTimestamp(Instant.now())
-						.setThumbnail(event.getUser().getAvatarUrl())
-						.addField("Balance", String.format("`%,d`", account.getBalance()), false)
-						.addBlankField(false)
-						.addField("Recent Transactions", "```" + transactionsText + "```", false)
-						.setFooter("User ID: " + account.getUserId());
+
+			EmbedBuilder embedBuilder = new EmbedBuilder()
+					.setTitle("Account Information for " + event.getUser().getAsTag())
+					.setTimestamp(Instant.now())
+					.setThumbnail(event.getUser().getAvatarUrl())
+					.addField("Balance", String.format("`%,d`", account.getBalance()), false)
+					.setFooter("User ID: " + account.getUserId());
+			if (!transactions.isEmpty()) {
+				Executors.newSingleThreadExecutor().submit(() -> {
+					try {
+						this.getTransactionsString(transactions, event, account).thenAccept(s -> {
+							embedBuilder.addField("Recent Transactions", "```" + s + "```", false);
+							event.getHook().sendMessageEmbeds(embedBuilder.build()).queue();
+						}).get();
+					} catch (InterruptedException | ExecutionException e) {
+						e.printStackTrace();
+					}
+				});
+			} else {
 				event.getHook().sendMessageEmbeds(embedBuilder.build()).queue();
-			});
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			event.getHook().sendMessage("Error: " + e.getMessage()).queue();
 		}
+	}
+
+	private CompletableFuture<String> getTransactionsString(List<Transaction> transactions, SlashCommandEvent event, Account account) {
+		List<RestAction<User>> userRestActions = new ArrayList<>();
+		for (var t : transactions) {
+			if (t.getFromUserId() != null) {
+				userRestActions.add(event.getJDA().retrieveUserById(t.getFromUserId()));
+			}
+			if (t.getToUserId() != null) {
+				userRestActions.add(event.getJDA().retrieveUserById(t.getToUserId()));
+			}
+		}
+		return RestAction.allOf(userRestActions).submit().thenApply(users -> {
+			Map<Long, User> usersMap = users.stream().distinct().collect(Collectors.toMap(ISnowflake::getIdLong, user -> user));
+			return transactions.stream()
+					.map(t -> {
+						String value = String.format("%,d", t.getValue());
+						String otherUserString;
+						if (t.getFromUserId() != null && t.getFromUserId().equals(account.getUserId())) {
+							value = "- " + value;
+							otherUserString = (usersMap.get(t.getToUserId()) == null ? "System" : usersMap.get(t.getToUserId()).getAsTag());
+						} else {
+							value = "+ " + value;
+							otherUserString = (usersMap.get(t.getFromUserId()) == null ? "System" : usersMap.get(t.getFromUserId()).getAsTag());
+						}
+						value = " ".repeat(VALUE_BUFFER_SPACE - value.length()) + value;
+						otherUserString = " ".repeat(VALUE_BUFFER_SPACE - otherUserString.length()) + otherUserString;
+						return String.format("%s\n%s\n%s", t.getCreatedAt().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss 'UTC'")), value, otherUserString);
+					})
+					.collect(Collectors.joining("\n"));
+		});
 	}
 }
