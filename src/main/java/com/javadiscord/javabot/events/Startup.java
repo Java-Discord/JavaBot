3a88bc7374d9172ff012f7465fb6ff37fcaad19e
LoadingBG <troyankru@gmail.com>
2021-08-12 17:33:26 +0300

    Replace for loops with streams for readability

--- src/main/java/com/javadiscord/javabot/events/Startup.java
+++ src/main/java/com/javadiscord/javabot/events/Startup.java
@@ -3,20 +3,26 @@ package com.javadiscord.javabot.events;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+
 import com.javadiscord.javabot.Bot;
 import com.javadiscord.javabot.commands.other.Version;
 import com.javadiscord.javabot.other.Database;
 import com.javadiscord.javabot.other.Misc;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 
 public class Startup extends ListenerAdapter {
@@ -40,41 +46,32 @@ public class Startup extends ListenerAdapter {
             mongoClient = new MongoClient(uri);
 
             logger.info("Successfully connected to Database");
-
         } catch (Exception e) {
-
             logger.error("Couldn't connect to Database... Shutting down...");
             System.exit(0);
         }
 
-        try {
-            StringBuilder sb = new StringBuilder();
-            Scanner fReader = new Scanner(getClass().getClassLoader().getResourceAsStream("textfiles/startup.txt"));
-
-            while (fReader.hasNextLine()) {
-                sb.append(fReader.nextLine() + "\n");
-            }
-            System.out.println("\n" + sb.toString().replace("{!version}", new Version().getVersion()));
-
+        try (BufferedReader startupMsgReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("textfiles/startup.txt")))) {
+			String startup = startupMsgReader
+                    .lines()
+                    .collect(Collectors.joining("\n"))
+                    .replace("{!version}", new Version().getVersion());
+            System.out.println("\n" + startup);
         } catch (Exception e) { logger.error("* textfiles/startup.txt not found"); }
 
         try { TimeUnit.MILLISECONDS.sleep(500); }
         catch (InterruptedException e) { e.printStackTrace(); }
 
-        String[] guildOrder = new String[]{"648956210850299986", "675136900478140422", "861254598046777344"};
-        //                                        Java              Mount Everest™    JavaDiscord Emoji Server
+        String[] guildOrder = new String[] {"648956210850299986", "675136900478140422", "861254598046777344"};
+        //                                         Java              Mount Everest™    JavaDiscord Emoji Server
 
-        for (int i = 0; i < event.getJDA().getGuilds().size(); i++) {
-
-            try {
-                preferredGuild = event.getJDA().getGuildById(guildOrder[i]);
-                if (event.getJDA().getGuilds().contains(preferredGuild)) break;
-                else continue;
-
-            } catch (Exception ignored) {}
-        }
-
-        if (preferredGuild == null) preferredGuild = event.getJDA().getGuilds().get(0);
+        JDA jda = event.getJDA();
+        List<Guild> jdaGuilds = jda.getGuilds();
+        preferredGuild = Arrays.stream(guildOrder)
+                .map(jda::getGuildById)
+                .filter(jdaGuilds::contains)
+                .findFirst()
+                .orElse(jdaGuilds.get(0));
 
         logger.info("Bot is ready!");
         logger.info("Logged in as " + event.getJDA().getSelfUser().getAsTag());
@@ -82,12 +79,10 @@ public class Startup extends ListenerAdapter {
         logger.info("Preferred Guild: " + preferredGuild.getName());
         logger.info("Guilds: " + Misc.getGuildList(event.getJDA().getGuilds(), true, true));
 
-        String[] skipGuilds = new String[]{"861254598046777344", "813817075218776101"};
+        List<String> skipGuilds = List.of("861254598046777344", "813817075218776101");
         //                                JavaDiscord Emoji Server    Test-Server
-
-        for (var guild : event.getJDA().getGuilds()) {
-
-            if(Arrays.asList(skipGuilds).contains(guild.getId())) continue;
+        for (var guild : jdaGuilds) {
+            if (skipGuilds.contains(guild.getId())) continue;
 
             new Database().deleteOpenSubmissions(guild);
             new StarboardListener().updateAllSBM(event, guild);
