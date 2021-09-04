3f7803ec6f3621c6d0b533f9690f2202eadcc896
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-09-04 11:39:06 +0200

    Started refactor of purge command.

--- src/main/java/com/javadiscord/javabot/commands/moderation/Purge.java
+++ src/main/java/com/javadiscord/javabot/commands/moderation/Purge.java
@@ -1,27 +1,47 @@
 package com.javadiscord.javabot.commands.moderation;
 
+import com.javadiscord.javabot.Bot;
+import com.javadiscord.javabot.commands.Responses;
 import com.javadiscord.javabot.commands.SlashCommandHandler;
-import com.javadiscord.javabot.other.Constants;
-import com.javadiscord.javabot.other.Embeds;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageHistory;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
 
-import java.awt.*;
+import javax.annotation.Nullable;
 import java.util.List;
 
+/**
+ * This command deletes messages from a channel.
+ */
 public class Purge implements SlashCommandHandler {
 
     @Override
     public ReplyAction handle(SlashCommandEvent event) {
-
-        if (!event.getMember().hasPermission(Permission.MESSAGE_MANAGE)) {
-            return event.replyEmbeds(Embeds.permissionError("MESSAGE_MANAGE", event)).setEphemeral(Constants.ERR_EPHEMERAL);
+        Member member = event.getMember();
+        if (member == null) {
+            return Responses.warning(event, "This command can only be used in a guild.");
+        }
+        if (member.hasPermission(Permission.MESSAGE_MANAGE)) {
+            return Responses.warning(event, "You do not have the `MESSAGE_MANAGE` permission which is required to remove messages.");
         }
 
+        OptionMapping amountOption = event.getOption("amount");
+        OptionMapping userOption = event.getOption("user");
+        OptionMapping archiveOption = event.getOption("archive");
+
+        Long amount = (amountOption == null) ? null : amountOption.getAsLong();
+        User user = (userOption == null) ? null : userOption.getAsUser();
+        boolean archive = archiveOption != null && archiveOption.getAsBoolean();
+
+        if (amount != null && (amount < 1 || amount > 1_000_000)) {
+            return Responses.warning(event, "Invalid amount. If specified, should be between 1 and 1,000,000, inclusive.");
+        }
+        /*
         int amount = (int) event.getOption("amount").getAsLong();
         boolean nuke;
         try {
@@ -51,5 +71,25 @@ public class Purge implements SlashCommandHandler {
             } catch (IndexOutOfBoundsException | IllegalArgumentException e) {
                 return event.replyEmbeds(Embeds.purgeError(event)).setEphemeral(Constants.ERR_EPHEMERAL);
             }
+
+         */
+        Bot.asyncPool.submit(() -> this.purge(amount, user, archive, event.getTextChannel()));
+        StringBuilder sb = new StringBuilder();
+        sb.append(amount != null ? (amount > 1 ? amount + " messages " : "1 message ") : "All messages ");
+        if (user != null) {
+            sb.append("by the user ").append(user.getAsTag()).append(' ');
         }
+        sb.append("will be removed").append(archive ? " and placed in an archive." : '.');
+        return Responses.info(event, "Purge Started", sb.toString());
     }
+
+    private void purge(@Nullable Long amount, @Nullable User user, boolean archive, TextChannel channel) {
+        var history = channel.getHistory();
+        List<Message> messages;
+        long count = 0;
+        do {
+            messages = history.retrievePast(amount == null ? 100 : (int) Math.min(100, amount)).complete();
+            if (messages.isEmpty()) return;
+        } while ((amount != null && amount > count) || (amount == null && !messages.isEmpty()));
+    }
+}
