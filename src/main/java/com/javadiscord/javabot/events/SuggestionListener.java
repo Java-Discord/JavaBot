6b1c4f24e3af259bead9ceb182d49bec104c9911
Dynxsty <jasonlessenich@gmail.com>
2021-09-26 12:01:16 +0200

    added check for `MessageType.THREAD_CREATED`

--- src/main/java/com/javadiscord/javabot/events/SuggestionListener.java
+++ src/main/java/com/javadiscord/javabot/events/SuggestionListener.java
@@ -4,6 +4,7 @@ import com.javadiscord.javabot.Bot;
 import com.javadiscord.javabot.other.Constants;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageType;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -13,38 +14,34 @@ public class SuggestionListener extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        try { if (event.getMember().getUser().isBot() || event.getMember() == null) return; }
-        catch (NullPointerException ignored) { return; }
+        if (event.getAuthor().isBot() || event.getAuthor().isSystem()
+                || event.getMessage().getType() == MessageType.THREAD_CREATED) return;
 
-            if (event.getChannel().equals(Bot.config.get(event.getGuild()).getModeration().getSuggestionChannel())) {
+            if (!event.getChannel().equals(
+                    Bot.config.get(event.getGuild()).getModeration().getSuggestionChannel())) return;
 
-                EmbedBuilder eb = new EmbedBuilder()
+                var eb = new EmbedBuilder()
                         .setColor(Constants.GRAY)
                         .setImage(null)
                         .setAuthor(event.getAuthor().getAsTag() + " · Suggestion", null, event.getAuthor().getEffectiveAvatarUrl())
                         .setTimestamp(new Date().toInstant())
-                        .setDescription(event.getMessage().getContentRaw());
+                        .setDescription(event.getMessage().getContentRaw())
+                        .build();
 
                 if (!event.getMessage().getAttachments().isEmpty()) {
                     Message.Attachment attachment = event.getMessage().getAttachments().get(0);
-
                     try {
-                        event.getChannel().sendFile(attachment.retrieveInputStream().get(), "attachment." + attachment.getFileExtension()).setEmbeds(eb.build()).queue(message -> {
+                        event.getChannel().sendFile(attachment.retrieveInputStream().get(), "attachment." + attachment.getFileExtension()).setEmbeds(eb).queue(message -> {
                             message.addReaction(Constants.REACTION_UPVOTE).queue();
                             message.addReaction(Constants.REACTION_DOWNVOTE).queue();
                         });
-
                     } catch (Exception e) { event.getChannel().sendMessage(e.getMessage()).queue(); }
-
                 } else {
-
-                    event.getChannel().sendMessageEmbeds(eb.build()).queue(message -> {
+                    event.getChannel().sendMessageEmbeds(eb).queue(message -> {
                         message.addReaction(Constants.REACTION_UPVOTE).queue();
                         message.addReaction(Constants.REACTION_DOWNVOTE).queue();
                     });
                 }
-
                 event.getMessage().delete().queue();
-            }
         }
     }
