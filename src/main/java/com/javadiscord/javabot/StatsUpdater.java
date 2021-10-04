2f04aeefbd82d1840ab395534b86e95fd5caa3bb
Dynxsty <jasonlessenich@gmail.com>
2021-10-04 11:28:37 +0200

    made requested changes

--- src/main/java/com/javadiscord/javabot/StatsUpdater.java
+++ src/main/java/com/javadiscord/javabot/StatsUpdater.java
@@ -1,44 +1,37 @@
 package com.javadiscord.javabot;
 
+import com.javadiscord.javabot.commands.SlashCommandHandler;
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import javax.annotation.Nonnull;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Periodically updates the Stats Categories for each guild in a set interval.
+ * <p>
+ *     This updater should be added as an event listener to the bot, so that it
+ *     will automatically begin operation when the bot gives the ready event.
+ * </p>
+ */
 @Slf4j
 public class StatsUpdater extends ListenerAdapter {
-
-    private final ScheduledExecutorService threadPool = Executors.newSingleThreadScheduledExecutor();
-
-    private final long delay = 20;
-
-    private final TimeUnit delayUnit = TimeUnit.MINUTES;
-
-    private JDA jda;
-
     @Override
     public void onReady(ReadyEvent event) {
-        this.jda = event.getJDA();
-        threadPool.scheduleWithFixedDelay(() -> {
-            for (var guild : jda.getGuilds()) {
+        Bot.asyncPool.scheduleWithFixedDelay(() -> {
+            for (var guild : event.getJDA().getGuilds()) {
                 var config = Bot.config.get(guild).getStats();
                 if (
                         config.getCategoryId() == 0 ||
                         config.getMemberCountMessageTemplate() == null
                 ) continue;
-
                 String text = config.getMemberCountMessageTemplate()
                         .replace("{!membercount}", String.valueOf(guild.getMemberCount()))
                         .replace("{!server}", guild.getName());
-                config.getStatsCategory().getManager().setName(text).queue();
+                config.getCategory().getManager().setName(text).queue();
             }
             log.info("Successfully updated Stats Categories");
-        }, 0, this.delay, this.delayUnit);
+        }, 0, 20, TimeUnit.MINUTES);
     }
 }
--- src/main/java/com/javadiscord/javabot/properties/config/guild/StatsConfig.java
+++ src/main/java/com/javadiscord/javabot/properties/config/guild/StatsConfig.java
@@ -5,14 +5,15 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import net.dv8tion.jda.api.entities.Category;
 
+import java.util.concurrent.TimeUnit;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class StatsConfig extends GuildConfigItem {
 	private long categoryId;
 	private String memberCountMessageTemplate;
 
-	public Category getStatsCategory() {
+	public Category getCategory() {
 		return getGuild().getCategoryById(this.categoryId);
 	}
-
 }
