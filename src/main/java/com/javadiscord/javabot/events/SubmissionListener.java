ff5b25bfddba6658f9bd3b48fa50a2b06df94bd6
Dynxsty <jasonlessenich@gmail.com>
2021-10-04 15:45:49 +0200

    fixed Submissions & removed unnecessary usage of `.editOriginalEmbeds()`

--- src/main/java/com/javadiscord/javabot/events/SubmissionListener.java
+++ src/main/java/com/javadiscord/javabot/events/SubmissionListener.java
@@ -25,73 +25,58 @@ import static com.mongodb.client.model.Filters.eq;
 
 public class SubmissionListener extends ListenerAdapter {
 
-    public void dmSubmissionSend (ButtonClickEvent event, String text) {
-
+    public void dmSubmissionSend(ButtonClickEvent event, String text) {
         Guild guild = preferredGuild;
-
         MongoDatabase database = mongoClient.getDatabase("other");
 
+        var config = Bot.config.get(guild);
+
         var eb = new EmbedBuilder()
-                .setColor(Color.decode(
-                        Bot.config.get(event.getGuild()).getSlashCommand().getWarningColor()))
+                .setColor(Color.decode(config.getSlashCommand().getDefaultColor()))
                 .setAuthor("Submission by " + event.getUser().getAsTag(), null, event.getUser().getEffectiveAvatarUrl())
                 .setDescription(text)
                 .setFooter("ID: " + event.getUser().getId())
                 .setTimestamp(new Date().toInstant())
                 .build();
 
-        Bot.config.get(guild).getQotw().getSubmissionChannel().sendMessageEmbeds(eb).setActionRows(ActionRow.of(
+        config.getQotw().getSubmissionChannel().sendMessageEmbeds(eb).setActionRows(ActionRow.of(
                 Button.success("submission:approve:" + event.getUser().getId(), "Approve"),
                 Button.danger("submission:decline:" + event.getUser().getId(), "Decline"),
                 Button.secondary("submission:delete:" + event.getUser().getId(), "🗑️")))
                 .queue(m -> {
-
-                    MongoCollection<Document> submission_messages = database.getCollection("submission_messages");
-
-                    Document doc = new Document()
+                    MongoCollection<Document> collection = database.getCollection("submission_messages");
+                    collection.insertOne(new Document()
                             .append("guild_id", m.getGuild().getId())
                             .append("channel_id", m.getChannel().getId())
                             .append("message_id", m.getId())
-                            .append("user_id", event.getUser().getId());
-
-                    submission_messages.insertOne(doc);
+                            .append("user_id", event.getUser().getId()));
                 });
-
-        event.getHook().editOriginalEmbeds(event.getMessage().getEmbeds().get(0))
-                .setActionRows(ActionRow.of(
-                        Button.success("dm-submission:send:" + event.getUser().getId(), "Submission sent").asDisabled()))
-                .queue();
+        event.getHook().editOriginalComponents()
+                .setActionRows(ActionRow.of(Button.success("dm-submission:send:" + event.getUser().getId(),
+                        "Submission sent").asDisabled())).queue();
     }
 
     public void dmSubmissionCancel (ButtonClickEvent event) {
-
-        event.getHook().editOriginalEmbeds(event.getMessage().getEmbeds().get(0))
-                .setActionRows(ActionRow.of(
-                        Button.danger("dm-submission:cancel:" + event.getUser().getId(), "Process canceled").asDisabled())
-                ).queue();
+        event.getHook().editOriginalComponents()
+                .setActionRows(ActionRow.of(Button.danger("dm-submission:cancel:" + event.getUser().getId(),
+                        "Process canceled").asDisabled())).queue();
     }
 
     public void submissionApprove (ButtonClickEvent event, String userID) {
-
         new Correct().correct(event.getGuild(), event.getGuild().retrieveMemberById(userID).complete());
 
-        event.getHook().editOriginalEmbeds(event.getMessage().getEmbeds().get(0))
-                .setActionRows(ActionRow.of(
-                        Button.success("submission:approve:" + userID, "Approved by " + event.getMember().getUser().getAsTag()).asDisabled())
-                ).queue();
+        event.getHook().editOriginalComponents()
+                .setActionRows(ActionRow.of(Button.success("submission:approve:" + userID,
+                        "Approved by " + event.getMember().getUser().getAsTag()).asDisabled())).queue();
     }
 
     public void submissionDecline (ButtonClickEvent event) {
-
-        event.getHook().editOriginalEmbeds(event.getMessage().getEmbeds().get(0))
-                .setActionRows(ActionRow.of(
-                        Button.danger("submission:decline:" + event.getUser().getId(), "Declined by " + event.getMember().getUser().getAsTag()).asDisabled())
-                )
-                .queue();
+        event.getHook().editOriginalComponents()
+                .setActionRows(ActionRow.of(Button.danger("submission:decline:" + event.getUser().getId(),
+                        "Declined by " + event.getMember().getUser().getAsTag()).asDisabled())).queue();
     }
 
     public void submissionDelete (ButtonClickEvent event) {
-
         event.getHook().deleteOriginal().queue();
     }
 
@@ -101,48 +86,41 @@ public class SubmissionListener extends ListenerAdapter {
 
         String message = event.getMessage().getContentDisplay();
         Guild guild = Startup.preferredGuild;
+        var config = Bot.config.get(guild);
 
-            if (!Bot.config.get(guild).getQotw().isDmEnabled()) return;
-                try {
-                    EmbedBuilder submissionEb = new EmbedBuilder()
-                            .setColor(Color.decode(
-                                    Bot.config.get(event.getAuthor().getMutualGuilds().get(0)).getSlashCommand().getDefaultColor()))
-                            .setAuthor("Question of the Week | Submission", null, event.getAuthor().getEffectiveAvatarUrl())
-                            .setDescription(message)
-                            .addField("Current Guild", guild.getName() + " `(" + guild.getId() + ")`", false)
-                            .setFooter("NOTE: spamming submissions may result in a warn")
-                            .setTimestamp(new Date().toInstant());
+        if (!config.getQotw().isDmEnabled()) return;
 
-                    MongoDatabase database = mongoClient.getDatabase("other");
-                    MongoCollection<Document> collection = database.getCollection("open_submissions");
+        var eb = new EmbedBuilder()
+                .setColor(Color.decode(config.getSlashCommand().getDefaultColor()))
+                .setAuthor("Question of the Week | Submission", null, event.getAuthor().getEffectiveAvatarUrl())
+                .setDescription(message)
+                .addField("Current Guild", guild.getName() + " `(" + guild.getId() + ")`", false)
+                .setFooter("NOTE: spamming submissions may result in a warn")
+                .setTimestamp(new Date().toInstant());
 
-                    Document document = collection.find(eq("guild_id", guild.getId())).first();
+        MongoDatabase database = mongoClient.getDatabase("other");
+        MongoCollection<Document> collection = database.getCollection("open_submissions");
 
-                    if (!(document == null)) {
-                        JsonObject root = JsonParser.parseString(document.toJson()).getAsJsonObject();
-                        String messageId = root.get("message_id").getAsString();
-                        Message msg = event.getAuthor().openPrivateChannel().complete().retrieveMessageById(messageId).complete();
+        Document document = collection.find(eq("guild_id", guild.getId())).first();
 
-                        msg.editMessageEmbeds(msg.getEmbeds().get(0))
-                                .setActionRows(ActionRow.of(
-                                        Button.danger("dm-submission:canceled:" + event.getAuthor().getId(), "Process canceled").asDisabled()))
-                                .queue();
-                        collection.deleteOne(document);
-                    }
+        if (!(document == null)) {
+            JsonObject root = JsonParser.parseString(document.toJson()).getAsJsonObject();
+            String messageId = root.get("message_id").getAsString();
+            Message msg = event.getAuthor().openPrivateChannel().complete().retrieveMessageById(messageId).complete();
 
-                    event.getChannel().sendMessageEmbeds(submissionEb.build()).setActionRows(ActionRow.of(
-                            Button.success("dm-submission:send:" + event.getAuthor().getId(), "Send Submission"),
-                            Button.danger("dm-submission:cancel:" + event.getAuthor().getId(), "Cancel"))).queue(
-                                m -> {
-                                    Document doc = new Document()
-                                            .append("guild_id", guild.getId())
-                                            .append("message_id", m.getId())
-                                            .append("user_id", event.getAuthor().getId())
-                                            .append("text", message);
-
-                                    collection.insertOne(doc);
-                                });
-                } catch (NullPointerException e) { e.printStackTrace(); }
+            msg.editMessageComponents()
+                    .setActionRows(ActionRow.of(Button.danger("dm-submission:canceled:" + event.getAuthor().getId(),
+                                    "Process canceled").asDisabled())).queue();
+            collection.deleteOne(document);
+        }
+        event.getChannel().sendMessageEmbeds(eb.build()).setActionRows(ActionRow.of(
+                Button.success("dm-submission:send:" + event.getAuthor().getId(), "Send Submission"),
+                Button.danger("dm-submission:cancel:" + event.getAuthor().getId(), "Cancel"))).queue(
+                m -> collection.insertOne(new Document()
+                        .append("guild_id", guild.getId())
+                        .append("message_id", m.getId())
+                        .append("user_id", event.getAuthor().getId())
+                        .append("text", message)));
             }
     }
 
