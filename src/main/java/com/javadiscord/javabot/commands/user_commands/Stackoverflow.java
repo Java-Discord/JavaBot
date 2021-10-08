5cef673b3a187484645c4654661daa1c28fe198f
Sahil Asopa <sahilasopa12@gmail.com>
2021-10-08 15:15:10 +0530

    commit

--- src/main/java/com/javadiscord/javabot/commands/user_commands/Stackoverflow.java
+++ src/main/java/com/javadiscord/javabot/commands/user_commands/Stackoverflow.java
@@ -1,2 +1,30 @@
-package com.javadiscord.javabot.commands.user_commands;public class Stackoverflow {
+package com.javadiscord.javabot.commands.user_commands;
+
+import com.javadiscord.javabot.commands.SlashCommandHandler;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class Stackoverflow implements SlashCommandHandler {
+    @Override
+    public ReplyAction handle(SlashCommandEvent event) {
+        String query = "";
+        String url;
+        try {
+            query = URLEncoder.encode(Objects.requireNonNull(event.getOption("query")).getAsString(), StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        query = query.replace(" ", "+");
+        if (!query.contains("java")) {
+            url = "www.google.com/search?q=".concat(query).concat("+in+java+site%3Astackoverflow.com");
+        } else {
+            url = "www.google.com/search?q=".concat(query).concat("+site%3Astackoverflow.com");
+        }
+        return event.reply(url);
+    }
 }
