f4953d8a520b9e192fd1b18777b8bbc688e9ee18
Kemikals <Waitinferage21@gmail.com>
2021-10-10 13:05:00 -1000

    change indentation for files

--- src/main/java/com/javadiscord/javabot/help/UnreserveCommandHandler.java
+++ src/main/java/com/javadiscord/javabot/help/UnreserveCommandHandler.java
@@ -16,39 +16,39 @@ import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
  * immediately unreserve them, instead of waiting for a timeout.
  */
 public class UnreserveCommandHandler implements SlashCommandHandler {
-    @Override
-    public ReplyAction handle(SlashCommandEvent event) {
-        var channel = event.getTextChannel();
-        var config = Bot.config.get(event.getGuild()).getHelp();
-        var channelManager = new HelpChannelManager(config);
-        var owner = channelManager.getReservedChannelOwner(channel);
-        if (isEligibleToBeUnreserved(event, channel, config, owner)) {
-            channelManager.unreserveChannel(channel).queue();
-            return Responses.success(event, "Channel Unreserved", "The channel has been unreserved.");
-        }
-        return Responses.warning(event, "Could not unreserve this channel. This command only works in help channels you've reserved.");
-    }
+	@Override
+	public ReplyAction handle(SlashCommandEvent event) {
+		var channel = event.getTextChannel();
+		var config = Bot.config.get(event.getGuild()).getHelp();
+		var channelManager = new HelpChannelManager(config);
+		var owner = channelManager.getReservedChannelOwner(channel);
+		if (isEligibleToBeUnreserved(event, channel, config, owner)) {
+			channelManager.unreserveChannel(channel).queue();
+			return Responses.success(event, "Channel Unreserved", "The channel has been unreserved.");
+		}
+		return Responses.warning(event, "Could not unreserve this channel. This command only works in help channels you've reserved.");
+	}
 
-    private boolean isEligibleToBeUnreserved(SlashCommandEvent event, TextChannel channel, HelpConfig config, User owner) {
-        return channelIsInReservedCategory(channel, config) &&
-                (isUserWhoReservedChannel(event, owner) || memberHasHelperRole(event) || memberHasStaffRole(event));
-    }
+	private boolean isEligibleToBeUnreserved(SlashCommandEvent event, TextChannel channel, HelpConfig config, User owner) {
+		return channelIsInReservedCategory(channel, config) &&
+				(isUserWhoReservedChannel(event, owner) || memberHasHelperRole(event) || memberHasStaffRole(event));
+	}
 
-    private boolean channelIsInReservedCategory(TextChannel channel, HelpConfig config) {
-        return config.getReservedChannelCategory().equals(channel.getParent());
-    }
+	private boolean channelIsInReservedCategory(TextChannel channel, HelpConfig config) {
+		return config.getReservedChannelCategory().equals(channel.getParent());
+	}
 
-    private boolean isUserWhoReservedChannel(SlashCommandEvent event, User owner) {
-        return event.getUser().equals(owner);
-    }
+	private boolean isUserWhoReservedChannel(SlashCommandEvent event, User owner) {
+		return event.getUser().equals(owner);
+	}
 
-    private boolean memberHasStaffRole(SlashCommandEvent event) {
-        return event.getMember() != null &&
-                event.getMember().getRoles().contains(Bot.config.get(event.getGuild()).getModeration().getStaffRole());
-    }
+	private boolean memberHasStaffRole(SlashCommandEvent event) {
+		return event.getMember() != null &&
+				event.getMember().getRoles().contains(Bot.config.get(event.getGuild()).getModeration().getStaffRole());
+	}
 
-    private boolean memberHasHelperRole(SlashCommandEvent event) {
-        return event.getMember() != null &&
-                event.getMember().getRoles().contains(Bot.config.get(event.getGuild()).getHelp().getHelperRole());
-    }
+	private boolean memberHasHelperRole(SlashCommandEvent event) {
+		return event.getMember() != null &&
+				event.getMember().getRoles().contains(Bot.config.get(event.getGuild()).getHelp().getHelperRole());
+	}
 }
