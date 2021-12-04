410aa2928d6ed00e2a8f64ea8b44bafef23c52c9
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-12-04 10:31:21 +0100

    Added help guidelines command.

---
+++ src/main/java/com/javadiscord/javabot/service/help/HelpGuidelinesCommandHandler.java
@@ -0,0 +1,22 @@
+package com.javadiscord.javabot.service.help;
+
+import com.javadiscord.javabot.commands.ResponseException;
+import com.javadiscord.javabot.commands.SlashCommandHandler;
+import com.javadiscord.javabot.utils.StringResourceCache;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
+
+/**
+ * Shows the server's help-guidelines.
+ */
+public class HelpGuidelinesCommandHandler implements SlashCommandHandler {
+	@Override
+	public ReplyAction handle(SlashCommandEvent event) throws ResponseException {
+		var embed = new EmbedBuilder()
+				.setTitle("Help Guidelines")
+				.setDescription(StringResourceCache.load("/help/guidelines.txt"))
+				.setImage("https://cdn.discordapp.com/attachments/744899463591624815/895244046027608074/unknown.png");
+		return event.replyEmbeds(embed.build());
+	}
+}
