63d161b3f334c546fe085cf776ec8cc6034c711e
MoonTM <benzweitacc@gmail.com>
2021-12-16 22:01:51 +0100

    Made Requested Changes

--- src/main/java/com/javadiscord/javabot/commands/user_commands/Regex.java
+++ src/main/java/com/javadiscord/javabot/commands/user_commands/Regex.java
@@ -1,7 +1,9 @@
 package com.javadiscord.javabot.commands.user_commands;
 
+import com.javadiscord.javabot.Bot;
 import com.javadiscord.javabot.commands.ResponseException;
 import com.javadiscord.javabot.commands.SlashCommandHandler;
+import com.javadiscord.javabot.data.properties.config.GuildConfig;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
@@ -10,26 +12,31 @@ import java.util.regex.Pattern;
 
 public class Regex implements SlashCommandHandler {
 
+    GuildConfig config;
+
     @Override
     public ReplyAction handle(SlashCommandEvent event) throws ResponseException {
+        config = Bot.config.get(event.getGuild());
 
         Pattern pattern = Pattern.compile(event.getOption("regex").getAsString());
         String string = event.getOption("string").getAsString();
 
-        return event.replyEmbeds(embedBuilder(pattern.matcher(string).matches(), pattern, string).build());
+        return event.replyEmbeds(buildRegexEmbed(pattern.matcher(string).matches(), pattern, string).build());
     }
 
-    private EmbedBuilder embedBuilder(boolean matches, Pattern pattern, String string){
+    private EmbedBuilder buildRegexEmbed(boolean matches, Pattern pattern, String string){
         EmbedBuilder eb = new EmbedBuilder()
                 .addField("Regex:", "```" + pattern.toString() + "```", true)
                 .addField("String:", "```" + string + "```", true);
 
+
+
         if (matches) {
             eb.setTitle("Regex Tester | ✓ Match");
-            eb.setColor(0x30bf56);
+            eb.setColor(config.getSlashCommand().getSuccessColor());
         } else {
             eb.setTitle("Regex Tester | ✗ No Match");
-            eb.setColor(0xd42f47);
+            eb.setColor(config.getSlashCommand().getErrorColor());
         }
 
         return eb;
