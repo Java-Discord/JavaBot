a3b7a07de734b6ecae3854d2998555ff0809d111
Sahil Asopa <sahilasopa12@gmail.com>
2022-01-06 16:36:35 +0530

    Added calls to AutoFetch api

--- src/main/java/net/javadiscord/javabot/systems/moderation/AutoMod.java
+++ src/main/java/net/javadiscord/javabot/systems/moderation/AutoMod.java
@@ -15,11 +15,16 @@ import net.javadiscord.javabot.util.Misc;
 import net.javadiscord.javabot.util.StringResourceCache;
 
 import javax.annotation.Nonnull;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,10 +44,19 @@ public class AutoMod extends ListenerAdapter {
 
 	public AutoMod() {
 		try {
-			spamUrls = Arrays.stream(StringResourceCache.load("/spamLinks.txt").split(System.lineSeparator())).toList();
-		} catch (Exception e) {
+			URL url = new URL("https://raw.githubusercontent.com/DevSpen/scam-links/master/src/links.txt");
+			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+			InputStream stream = connection.getInputStream();
+			String response = new Scanner(stream).useDelimiter("\\A").next();
+			spamUrls = List.of(response.split("\n"));
+		} catch (IOException e) {
 			e.printStackTrace();
-			spamUrls = List.of();
+			try {
+				spamUrls = Arrays.stream(StringResourceCache.load("/spamLinks.txt").split(System.lineSeparator())).toList();
+			} catch (Exception exception) {
+				exception.printStackTrace();
+				spamUrls = List.of();
+			}
 		}
 		log.info("Loaded {} spam URLs!", spamUrls.size());
 	}
