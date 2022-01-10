9df81676418911dfc175b71a96386cb3c73a89fe
Dynxsty <jasonlessenich@gmail.com>
2022-01-10 16:34:45 +0100

    added custom Timeouts

--- src/main/java/net/javadiscord/javabot/systems/moderation/timeout/subcommands/AddTimeoutSubCommand.java
+++ src/main/java/net/javadiscord/javabot/systems/moderation/timeout/subcommands/AddTimeoutSubCommand.java
@@ -2,6 +2,7 @@ package net.javadiscord.javabot.systems.moderation.timeout.subcommands;
 
 
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
 import net.javadiscord.javabot.command.Responses;
@@ -9,6 +10,7 @@ import net.javadiscord.javabot.command.SlashCommandHandler;
 import net.javadiscord.javabot.systems.moderation.ModerationService;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 
 public class AddTimeoutSubCommand implements SlashCommandHandler {
@@ -17,14 +19,17 @@ public class AddTimeoutSubCommand implements SlashCommandHandler {
     public ReplyAction handle(SlashCommandEvent event) {
         var userOption = event.getOption("user");
         var reasonOption = event.getOption("reason");
-        var durationOption = event.getOption("duration");
-        if (userOption == null || reasonOption == null || durationOption == null) {
+        var durationAmountOption = event.getOption("duration-amount");
+        var durationTimeUnitOption = event.getOption("duration-timeunit");
+        if (userOption == null || reasonOption == null || durationAmountOption == null || durationTimeUnitOption == null) {
             return Responses.error(event, "Missing required Arguments.");
         }
         var member = userOption.getAsMember();
         var reason = reasonOption.getAsString();
-        var durations = durationOption.getAsString().split(":");
-        var duration = Duration.of(Integer.parseInt(durations[0]), TimeUnit.valueOf(durations[1]).toChronoUnit());
+        var duration = Duration.of(durationAmountOption.getAsLong(), ChronoUnit.valueOf(durationTimeUnitOption.getAsString()));
+        if (duration.toSeconds() > (Member.MAX_TIME_OUT_LENGTH * 24 * 60 * 60)) {
+            return Responses.error(event, String.format("You cannot add a Timeout longer than %s days.", Member.MAX_TIME_OUT_LENGTH));
+        }
         var channel = event.getTextChannel();
         if (channel.getType() != ChannelType.TEXT) {
             return Responses.error(event, "This command can only be performed in a server text channel.");
