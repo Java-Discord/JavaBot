f08d1cf154e0f13f91c00493dd2b703a4d5ef091
Dynxsty <jasonlessenich@gmail.com>
2022-01-15 15:45:01 +0100

    replaced Text block with normal Strings

--- src/main/java/net/javadiscord/javabot/systems/qotw/SubmissionManager.java
+++ src/main/java/net/javadiscord/javabot/systems/qotw/SubmissionManager.java
@@ -28,26 +28,23 @@ public class SubmissionManager {
 			return Responses.warning(event.getHook(), "You're not eligible to create a new submission thread.");
 		}
 		config.getSubmissionChannel()
-				.createThreadChannel(String.format("Submission by %s | %s (%s)",
-								member.getEffectiveName(), member.getId(), questionNumber),
+				.createThreadChannel(
+						String.format("Submission by %s | %s (%s)", member.getEffectiveName(), member.getId(), questionNumber),
 						true).queue(
 						thread -> {
 							var manager = thread.getManager();
 							manager.setInvitable(false).queue();
 							manager.setAutoArchiveDuration(ThreadChannel.AutoArchiveDuration.TIME_1_WEEK).queue();
-							thread.sendMessageFormat(
-									"""
-											**Question of the Week #%s**
-
-											Hey, %s! Please submit your answer into this thread.
-											You can even send multiple messages, if you want to. This whole thread counts as your submission.
-											The %s will review your submission once a new question appears.""",
-									questionNumber, member.getAsMention(), config.getQOTWReviewRole().getAsMention()).queue();
+							thread.sendMessageFormat("**Question of the Week #%s**\n" +
+											"\nHey, %s! Please submit your answer into this thread." +
+											"\nYou can even send multiple messages, if you want to. This whole thread counts as your submission." +
+											"\nThe %s will review your submission once a new question appears.",
+									questionNumber, member.getAsMention(), config.getQOTWReviewRole().getAsMention())
+									.queue();
 						}, e -> log.error("Could not create submission thread for member {}. ", member.getUser().getAsTag(), e)
 				);
 		log.info("Opened new Submission Thread for User {}", member.getUser().getAsTag());
-		return Responses.success(event.getHook(),
-				"Submission Thread created",
+		return Responses.success(event.getHook(), "Submission Thread created",
 				"Successfully created a new private Thread for your submission.");
 	}
 
