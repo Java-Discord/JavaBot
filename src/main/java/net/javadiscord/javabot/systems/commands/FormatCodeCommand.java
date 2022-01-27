912df2243bdb2e68d7a3503ad678fb903a6a3181
Dynxsty <jasonlessenich@gmail.com>
2022-01-27 12:05:58 +0100

    fixed indentation and uncollapsed if-statements

--- src/main/java/net/javadiscord/javabot/systems/commands/FormatCodeCommand.java
+++ src/main/java/net/javadiscord/javabot/systems/commands/FormatCodeCommand.java
@@ -17,31 +17,36 @@ import java.time.Instant;
  * Command that allows members to format messages.
  */
 public class FormatCodeCommand implements SlashCommandHandler {
-    @Override
-    public ReplyAction handle(SlashCommandEvent event)  {
-        var idOption = event.getOption("message-id");
-        var formatOption = event.getOption("format");
-        String format = formatOption == null ? "java" : formatOption.getAsString();
-        long id;
+	@Override
+	public ReplyAction handle(SlashCommandEvent event) {
+		var idOption = event.getOption("message-id");
+		var formatOption = event.getOption("format");
+		String format = formatOption == null ? "java" : formatOption.getAsString();
+		long id;
         if (idOption == null) {
-            if (event.getChannel().hasLatestMessage()) id = event.getChannel().getLatestMessageIdLong();
-            else return Responses.error(event, "Missing required arguments.");
-        } else id = idOption.getAsLong();
-        var slashConfig = Bot.config.get(event.getGuild()).getSlashCommand();
-        event.getTextChannel().retrieveMessageById(id).queue(
-                m -> event.getHook().sendMessageEmbeds(buildFormatCodeEmbed(m, m.getAuthor(), format, slashConfig)).queue(),
-                e -> Responses.error(event.getHook(), "Could not retrieve message.").queue());
-        return event.deferReply();
-    }
+            if (event.getChannel().hasLatestMessage()) {
+                id = event.getChannel().getLatestMessageIdLong();
+            } else {
+                return Responses.error(event, "Missing required arguments.");
+            }
+        } else {
+            id = idOption.getAsLong();
+        }
+		var slashConfig = Bot.config.get(event.getGuild()).getSlashCommand();
+		event.getTextChannel().retrieveMessageById(id).queue(
+				m -> event.getHook().sendMessageEmbeds(buildFormatCodeEmbed(m, m.getAuthor(), format, slashConfig)).queue(),
+				e -> Responses.error(event.getHook(), "Could not retrieve message.").queue());
+		return event.deferReply();
+	}
 
-    private MessageEmbed buildFormatCodeEmbed(Message message, User author, String format, SlashCommandConfig config) {
-        return new EmbedBuilder()
-                .setAuthor(author.getAsTag(), null, author.getEffectiveAvatarUrl())
-                .setTitle("Original Message", message.getJumpUrl())
-                .setColor(config.getDefaultColor())
-                .setDescription(String.format("```%s\n%s\n```", format, message.getContentRaw()))
-                .setFooter("Formatted as: " + format)
-                .setTimestamp(Instant.now())
-                .build();
-    }
+	private MessageEmbed buildFormatCodeEmbed(Message message, User author, String format, SlashCommandConfig config) {
+		return new EmbedBuilder()
+				.setAuthor(author.getAsTag(), null, author.getEffectiveAvatarUrl())
+				.setTitle("Original Message", message.getJumpUrl())
+				.setColor(config.getDefaultColor())
+				.setDescription(String.format("```%s\n%s\n```", format, message.getContentRaw()))
+				.setFooter("Formatted as: " + format)
+				.setTimestamp(Instant.now())
+				.build();
+	}
 }
