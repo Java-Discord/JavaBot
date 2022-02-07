f15c1650cf06696fea99cb0d8e9b84555facdf9d
MoonTM <benzweitacc@gmail.com>
2022-02-07 16:44:01 +0100

    Update RedeployCommand to jda5

--- src/main/java/net/javadiscord/javabot/systems/staff/RedeployCommand.java
+++ src/main/java/net/javadiscord/javabot/systems/staff/RedeployCommand.java
@@ -1,9 +1,9 @@
 package net.javadiscord.javabot.systems.staff;
 
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
-import net.javadiscord.javabot.command.SlashCommandHandler;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
+import net.javadiscord.javabot.command.interfaces.ISlashCommand;
 
 /**
  * Command that lets staff-members redeploy the bot.
@@ -14,9 +14,9 @@ import net.javadiscord.javabot.command.SlashCommandHandler;
  * I have explained how we do it in https://github.com/Java-Discord/JavaBot/pull/195
  */
 @Slf4j
-public class RedeployCommand implements SlashCommandHandler {
+public class RedeployCommand implements ISlashCommand {
 	@Override
-	public ReplyAction handle(SlashCommandEvent event) {
+	public ReplyCallbackAction handleSlashCommandInteraction(SlashCommandInteractionEvent event) {
 		log.warn("Redeploying... Requested by: " + event.getUser().getAsTag());
 		event.reply("Redeploying... this can take up to 2 Minutes.").queue();
 		System.exit(0);
