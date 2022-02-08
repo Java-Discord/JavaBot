ce997d7a7a0d12710a246bbe74b86078b2bd5b81
Dynxsty <jasonlessenich@gmail.com>
2022-02-08 17:44:20 +0100

    Simplified the Job Channel Listener

--- src/main/java/net/javadiscord/javabot/events/JobChannelVoteListener.java
+++ src/main/java/net/javadiscord/javabot/events/JobChannelVoteListener.java
@@ -1,12 +1,8 @@
 package net.javadiscord.javabot.events;
 
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.MessageReaction;
-import net.dv8tion.jda.api.events.message.GenericMessageEvent;
-import net.dv8tion.jda.api.events.message.react.GenericMessageReactionEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionRemoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.javadiscord.javabot.Bot;
 import org.jetbrains.annotations.NotNull;
@@ -17,43 +13,25 @@ import org.jetbrains.annotations.NotNull;
  */
 @Slf4j
 public class JobChannelVoteListener extends ListenerAdapter {
-
 	@Override
 	public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
-		onReactionEvent(event);
-	}
-
-	@Override
-	public void onMessageReactionRemove(@NotNull MessageReactionRemoveEvent event) {
-		onReactionEvent(event);
-	}
-
-	private boolean isInvalidEvent(GenericMessageEvent genericEvent) {
-		if (genericEvent instanceof MessageReactionAddEvent raEvent && raEvent.getUser() != null && (raEvent.getUser().isBot() || raEvent.getUser().isSystem())) {
-			return true;
-		}
-		if (genericEvent.getChannelType() == ChannelType.PRIVATE) return true;
-		return !genericEvent.getChannel().equals(Bot.config.get(genericEvent.getGuild()).getModeration().getJobChannel());
-	}
-
-	private void onReactionEvent(GenericMessageReactionEvent event) {
 		if (event.getUser() == null || event.getUser().isBot() || event.getUser().isSystem()) return;
-		if (isInvalidEvent(event)) return;
 		var config = Bot.config.get(event.getGuild());
 		if (!event.getReactionEmote().isEmoji() || !event.getReactionEmote().getAsReactionCode().equals(config.getEmote().getJobChannelVoteEmoji())) return;
+		if (!event.getTextChannel().equals(config.getModeration().getJobChannel())) return;
 		event.getChannel().retrieveMessageById(event.getMessageId()).queue(message -> {
-			int downvotes = message
+			int votes = message
 					.getReactions()
 					.stream()
 					.filter(reaction -> reaction.getReactionEmote().getName().equals(config.getEmote().getJobChannelVoteEmoji()))
 					.findFirst()
 					.map(MessageReaction::getCount)
 					.orElse(0);
-			if (downvotes >= config.getModeration().getJobChannelMessageDeleteThreshold()) {
+			if (votes >= config.getModeration().getJobChannelMessageDeleteThreshold()) {
 				message.delete().queue();
 				message.getAuthor().openPrivateChannel()
 						.queue(
-								s -> s.sendMessage(String.format("Your message in %s has been removed due to community feedback.", config.getModeration().getJobChannel().getAsMention())).queue(),
+								s -> s.sendMessageFormat("Your message in %s has been removed due to community feedback.", config.getModeration().getJobChannel().getAsMention()).queue(),
 								e -> {}
 						);
 			}
