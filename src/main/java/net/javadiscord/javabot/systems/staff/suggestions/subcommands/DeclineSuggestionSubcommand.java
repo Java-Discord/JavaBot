a95a5ef8b5b7830e715ac81fc647099e681f0241
Dynxsty <jasonlessenich@gmail.com>
2022-03-12 14:53:47 +0100

    Added predefined decline reasons

--- src/main/java/net/javadiscord/javabot/systems/staff/suggestions/subcommands/DeclineSuggestionSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/staff/suggestions/subcommands/DeclineSuggestionSubcommand.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.requests.restaction.WebhookMessageAction;
 import net.javadiscord.javabot.command.Responses;
 import net.javadiscord.javabot.data.config.GuildConfig;
@@ -18,9 +19,10 @@ import net.javadiscord.javabot.systems.staff.suggestions.SuggestionSubcommand;
 public class DeclineSuggestionSubcommand extends SuggestionSubcommand {
 	@Override
 	protected WebhookMessageAction<Message> handleSuggestionCommand(SlashCommandInteractionEvent event, Message message, GuildConfig config) {
+		String reason = event.getOption("reason", null, OptionMapping::getAsString);
 		MessageEmbed embed = message.getEmbeds().get(0);
 		message.clearReactions().queue();
-		MessageEmbed declineEmbed = this.buildSuggestionDeclineEmbed(event.getUser(), embed, config);
+		MessageEmbed declineEmbed = this.buildSuggestionDeclineEmbed(event.getUser(), embed, reason, config);
 		message.editMessageEmbeds(declineEmbed).queue(
 				edit -> edit.addReaction(config.getEmote().getFailureEmote()).queue(),
 				error -> Responses.error(event.getHook(), error.getMessage()).queue());
@@ -28,14 +30,15 @@ public class DeclineSuggestionSubcommand extends SuggestionSubcommand {
 				.addActionRows(this.getJumpButton(message));
 	}
 
-	private MessageEmbed buildSuggestionDeclineEmbed(User user, MessageEmbed embed, GuildConfig config) {
-		return new EmbedBuilder()
+	private MessageEmbed buildSuggestionDeclineEmbed(User user, MessageEmbed embed, String reason, GuildConfig config) {
+		EmbedBuilder builder = new EmbedBuilder()
 				.setColor(config.getSlashCommand().getErrorColor())
 				.setAuthor(embed.getAuthor().getName(), embed.getAuthor().getUrl(), embed.getAuthor().getIconUrl())
 				.setTitle("Suggestion Declined")
 				.setDescription(embed.getDescription())
 				.setTimestamp(embed.getTimestamp())
-				.setFooter("Declined by " + user.getAsTag())
-				.build();
+				.setFooter("Declined by " + user.getAsTag());
+		if (reason != null) builder.addField("Reason", reason, false);
+		return builder.build();
 	}
 }
