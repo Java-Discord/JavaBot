1ba2591d86da2f891a3048d104440cfac9a6fd96
Dynxsty <jasonlessenich@gmail.com>
2022-03-28 00:14:25 +0200

    bumped JDA to a newer version (Xirado's Fork)

--- src/main/java/net/javadiscord/javabot/systems/staff/self_roles/SelfRoleInteractionManager.java
+++ src/main/java/net/javadiscord/javabot/systems/staff/self_roles/SelfRoleInteractionManager.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.text.Modal;
+import net.dv8tion.jda.api.interactions.components.Modal;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.requests.restaction.WebhookMessageAction;
