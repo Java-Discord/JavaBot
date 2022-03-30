a89fb675cc369432c5ec277f94ec07132a05cc90
Dynxsty <jasonlessenich@gmail.com>
2022-03-30 12:40:47 +0200

    Improved visuals of `/qotw questions-queue list`

--- src/main/java/net/javadiscord/javabot/systems/qotw/subcommands/questions_queue/ListQuestionsSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/qotw/subcommands/questions_queue/ListQuestionsSubcommand.java
@@ -7,11 +7,12 @@ import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 import net.javadiscord.javabot.Bot;
 import net.javadiscord.javabot.command.Responses;
 import net.javadiscord.javabot.systems.qotw.dao.QuestionQueueRepository;
+import net.javadiscord.javabot.systems.qotw.model.QOTWQuestion;
 import net.javadiscord.javabot.systems.qotw.subcommands.QOTWSubcommand;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.time.format.DateTimeFormatter;
+import java.time.ZoneOffset;
 
 /**
  * Subcommand that allows staff-members to list QOTW Questions.
@@ -29,24 +30,25 @@ public class ListQuestionsSubcommand extends QOTWSubcommand {
 			}
 			page = userPage;
 		}
-
 		var questions = repository.getQuestions(guildId, page, 10);
 		EmbedBuilder embedBuilder = new EmbedBuilder()
-				.setTitle("QOTW Questions Queue");
+				.setAuthor(event.getUser().getAsTag(), null, event.getUser().getEffectiveAvatarUrl())
+				.setTitle("QOTW Questions Queue")
+				.setColor(Bot.config.get(event.getGuild()).getSlashCommand().getDefaultColor());
 		if (questions.isEmpty()) {
 			embedBuilder.setDescription("There are no questions in the queue.");
 			return event.replyEmbeds(embedBuilder.build());
 		}
 		Bot.asyncPool.submit(() -> {
-			for (var question : questions) {
+			for (QOTWQuestion question : questions) {
 				embedBuilder.addField(
 						String.valueOf(question.getId()),
 						String.format(
-								"> %s\nPriority: **%d**\nCreated by: %s\nCreated at: %s",
+								"> %s\nPriority: **%d**\nCreated by: %s\nCreated at: <t:%s:D>",
 								question.getText(),
 								question.getPriority(),
-								event.getJDA().retrieveUserById(question.getCreatedBy()).complete().getAsTag(),
-								question.getCreatedAt().format(DateTimeFormatter.ofPattern("d MMMM yyyy"))
+								event.getJDA().retrieveUserById(question.getCreatedBy()).complete().getAsMention(),
+								question.getCreatedAt().toEpochSecond(ZoneOffset.UTC)
 						),
 						false
 				);
