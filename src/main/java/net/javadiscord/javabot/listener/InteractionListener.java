904ba1ed7d168e87e24d677670655b97e2934c25
Dynxsty <jasonlessenich@gmail.com>
2022-03-30 13:45:58 +0200

    some more cleanup

--- src/main/java/net/javadiscord/javabot/listener/InteractionListener.java
+++ src/main/java/net/javadiscord/javabot/listener/InteractionListener.java
@@ -3,6 +3,7 @@ package net.javadiscord.javabot.listener;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.ThreadChannel;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -13,11 +14,8 @@ import net.javadiscord.javabot.systems.moderation.ModerationService;
 import net.javadiscord.javabot.systems.moderation.ReportCommand;
 import net.javadiscord.javabot.systems.qotw.submissions.SubmissionControlsManager;
 import net.javadiscord.javabot.systems.qotw.submissions.SubmissionManager;
-import net.javadiscord.javabot.systems.qotw.submissions.dao.QOTWSubmissionRepository;
 import net.javadiscord.javabot.systems.staff.self_roles.SelfRoleInteractionManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.sql.SQLException;
 
 /**
  * Listens for Interaction Events and handles them.
@@ -26,19 +24,34 @@ import java.sql.SQLException;
 public class InteractionListener extends ListenerAdapter {
 
 	@Override
+	public void onCommandAutoCompleteInteraction(@NotNull CommandAutoCompleteInteractionEvent event) {
+		// TODO: add autocomplete interactions (next pr)
+	}
+
+	/**
+	 * Handles the {@link ModalInteractionEvent} and executes the corresponding interaction, based on the id.
+	 *
+	 * @param event The {@link ModalInteractionEvent} that was fired.
+	 */
+	@Override
 	public void onModalInteraction(@NotNull ModalInteractionEvent event) {
-		if(event.getUser().isBot()) return;
+		if (event.getUser().isBot()) return;
 		String[] id = event.getModalId().split(":");
-		switch(id[0]) {
+		switch (id[0]) {
 			case "self-role" -> new SelfRoleInteractionManager().handleModalSubmit(event, id);
 			case "report" -> new ReportCommand().handleModalSubmit(event, id);
 			default -> Responses.error(event.getHook(), "Unknown Interaction").queue();
 		}
 	}
 
+	/**
+	 * Handles the {@link SelectMenuInteractionEvent} and executes the corresponding interaction, based on the id.
+	 *
+	 * @param event The {@link SelectMenuInteractionEvent} that was fired.
+	 */
 	@Override
 	public void onSelectMenuInteraction(SelectMenuInteractionEvent event) {
-		if(event.getUser().isBot()) return;
+		if (event.getUser().isBot()) return;
 		String[] id = event.getComponentId().split(":");
 		switch (id[0]) {
 			case "submission-controls-select" -> new SubmissionControlsManager(event.getGuild(), (ThreadChannel) event.getGuildChannel()).handleSelectMenus(id, event);
@@ -46,16 +59,24 @@ public class InteractionListener extends ListenerAdapter {
 		}
 	}
 
+	/**
+	 * Handles the {@link ButtonInteractionEvent} and executes the corresponding interaction, based on the id.
+	 *
+	 * @param event The {@link ButtonInteractionEvent} that was fired.
+	 */
 	@Override
 	public void onButtonInteraction(ButtonInteractionEvent event) {
-		if(event.getUser().isBot()) return;
+		if (event.getUser().isBot()) return;
 		String[] id = event.getComponentId().split(":");
 		var config = Bot.config.get(event.getGuild());
 		switch (id[0]) {
 			case "qotw-submission" -> {
-				var manager = new SubmissionManager(config.getQotw());
-				if (!id[1].isEmpty() && id[1].equals("delete")) manager.handleThreadDeletion(event);
-				else manager.handleSubmission(event, Integer.parseInt(id[1])).queue();
+				SubmissionManager manager = new SubmissionManager(config.getQotw());
+				if (!id[1].isEmpty() && id[1].equals("delete")) {
+					manager.handleThreadDeletion(event);
+				} else {
+					manager.handleSubmission(event, Integer.parseInt(id[1])).queue();
+				}
 			}
 			case "submission-controls" -> new SubmissionControlsManager(event.getGuild(), (ThreadChannel) event.getGuildChannel()).handleButtons(id, event);
 			case "resolve-report" -> new ReportCommand().markAsResolved(event, id[1]);
@@ -69,7 +90,6 @@ public class InteractionListener extends ListenerAdapter {
 
 	/**
 	 * Some utility methods for interactions.
-	 * + May be useful for Context Menu Interactions.
 	 *
 	 * @param id    The button's id, split by ":".
 	 * @param event The {@link ButtonInteractionEvent} that is fired upon use.
