0c09b53a5fd0f8fe1fcc524a71d4c3e34d2163a3
Dynxsty <jasonlessenich@gmail.com>
2022-04-01 01:54:56 +0200

    fixed `IAutocomplete` Javadoc

--- src/main/java/net/javadiscord/javabot/command/interfaces/IAutocomplete.java
+++ src/main/java/net/javadiscord/javabot/command/interfaces/IAutocomplete.java
@@ -5,17 +5,6 @@ import net.dv8tion.jda.api.requests.restaction.interactions.AutoCompleteCallback
 
 /**
  * Implement this interface to declare that your class handles Autocomplete functionality.
- * <p>
- * <strong>All implementing classes should have a public, no-args
- * constructor.</strong>
- * </p>
- * <p>
- * To enable this handler for a particular slash command option, navigate to that
- * command's entry in the corresponding YAML-file, and add the following property:
- * <pre><code>
- * handler: com.javadiscord.javabot.commands.MyFullHandlerClassName
- *     </code></pre>
- * </p>
  */
 public interface IAutocomplete {
 	AutoCompleteCallbackAction handleAutocomplete(CommandAutoCompleteInteractionEvent event);
