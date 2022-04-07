b785ff83c79d9ce6f69e204faa8024d43ab5cb55
Dynxsty <jasonlessenich@gmail.com>
2022-04-07 07:42:48 +0200

    improved code formatting for `/format-code`

--- src/main/java/net/javadiscord/javabot/systems/commands/FormatCodeCommand.java
+++ src/main/java/net/javadiscord/javabot/systems/commands/FormatCodeCommand.java
@@ -1,20 +1,19 @@
 package net.javadiscord.javabot.systems.commands;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 import net.javadiscord.javabot.Bot;
 import net.javadiscord.javabot.command.Responses;
 import net.javadiscord.javabot.command.interfaces.MessageContextCommand;
 import net.javadiscord.javabot.command.interfaces.SlashCommand;
-import net.javadiscord.javabot.data.config.guild.SlashCommandConfig;
+import net.javadiscord.javabot.util.StringUtils;
 
-import java.time.Instant;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Command that allows members to format messages.
@@ -27,20 +26,23 @@ public class FormatCodeCommand implements SlashCommand, MessageContextCommand {
 		var slashConfig = Bot.config.get(event.getGuild()).getSlashCommand();
 		if (idOption == null) {
 			event.getChannel().getHistory()
-				.retrievePast(10)
-				.queue(messages -> {
-					Message target = null;
-					Collections.reverse(messages);
-					for (Message m : messages) {
-						if (!m.getAuthor().isBot()) target = m;
-					}
-					if (target != null) event.getHook().sendMessageEmbeds(buildFormatCodeEmbed(target, format, slashConfig)).queue();
-					else Responses.error(event.getHook(), "Missing required arguments.").queue();
-				});
+					.retrievePast(10)
+					.queue(messages -> {
+						Message target = null;
+						Collections.reverse(messages);
+						for (Message m : messages) {
+							if (!m.getAuthor().isBot()) target = m;
+						}
+						if (target != null) {
+							event.getHook().sendMessageFormat("```%s\n%s\n```", format, StringUtils.standardSanitizer().compute(target.getContentRaw())).queue();
+						} else {
+							Responses.error(event.getHook(), "Missing required arguments.").queue();
+						}
+					});
 		} else {
 			long messageId = idOption.getAsLong();
 			event.getTextChannel().retrieveMessageById(messageId).queue(
-					m -> event.getHook().sendMessageEmbeds(buildFormatCodeEmbed(m, format, slashConfig)).queue(),
+					m -> event.getHook().sendMessageFormat("```%s\n%s\n```", format, StringUtils.standardSanitizer().compute(m.getContentRaw())).queue(),
 					e -> Responses.error(event.getHook(), "Could not retrieve message with id: " + messageId).queue());
 		}
 		return event.deferReply();
@@ -48,17 +50,8 @@ public class FormatCodeCommand implements SlashCommand, MessageContextCommand {
 
 	@Override
 	public ReplyCallbackAction handleMessageContextCommandInteraction(MessageContextInteractionEvent event) {
-		return event.replyEmbeds(buildFormatCodeEmbed(event.getTarget(), "java", Bot.config.get(event.getGuild()).getSlashCommand()));
-	}
-
-	private MessageEmbed buildFormatCodeEmbed(Message message, String format, SlashCommandConfig config) {
-		return new EmbedBuilder()
-				.setAuthor(message.getAuthor().getAsTag(), null, message.getAuthor().getEffectiveAvatarUrl())
-				.setTitle("Original Message", message.getJumpUrl())
-				.setColor(config.getDefaultColor())
-				.setDescription(String.format("```%s\n%s\n```", format, message.getContentRaw()))
-				.setFooter("Formatted as: " + format)
-				.setTimestamp(Instant.now())
-				.build();
+		return event.replyFormat("```java\n%s\n```", StringUtils.standardSanitizer().compute(event.getTarget().getContentRaw()))
+				.allowedMentions(List.of())
+				.addActionRow(Button.link(event.getTarget().getJumpUrl(), "View Original"));
 	}
 }
