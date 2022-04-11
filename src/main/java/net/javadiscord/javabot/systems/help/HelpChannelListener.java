4f4817de084b85206e957f5a6345a40076660185
Dynxsty <jasonlessenich@gmail.com>
2022-04-12 01:48:08 +0200

    Added null-check for Open Help Channel Category

--- src/main/java/net/javadiscord/javabot/systems/help/HelpChannelListener.java
+++ src/main/java/net/javadiscord/javabot/systems/help/HelpChannelListener.java
@@ -1,5 +1,7 @@
 package net.javadiscord.javabot.systems.help;
 
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -13,19 +15,24 @@ import java.sql.SQLException;
  * This listener is responsible for handling messages that are sent in one or
  * more designated help channels.
  */
+@Slf4j
 public class HelpChannelListener extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
 		if (event.getAuthor().isBot() || event.getAuthor().isSystem() || event.getChannelType() != ChannelType.TEXT) {
 			return;
 		}
-
 		var config = Bot.config.get(event.getGuild()).getHelp();
 		TextChannel channel = event.getTextChannel();
 		var manager = new HelpChannelManager(config);
 
 		// If a message was sent in an open text channel, reserve it.
-		if (config.getOpenChannelCategory().equals(channel.getParentCategory())) {
+		Category openChannelCategory = config.getOpenChannelCategory();
+		if (openChannelCategory == null) {
+			log.error("Could not find Open Help Category for Guild {}", event.getGuild().getName());
+			return;
+		}
+		if (openChannelCategory.equals(channel.getParentCategory())) {
 			if (manager.mayUserReserveChannel(event.getAuthor())) {
 				try {
 					manager.reserve(channel, event.getAuthor(), event.getMessage());
