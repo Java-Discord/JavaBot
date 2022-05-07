fbd303add2873b1d003140a680ad20dd54f2107c
MoonTM <benzweitacc@gmail.com>
2022-05-07 17:26:09 +0200

    Made Dynxsty's Changes

--- src/main/java/net/javadiscord/javabot/listener/PingableNameListener.java
+++ src/main/java/net/javadiscord/javabot/listener/PingableNameListener.java
@@ -3,9 +3,12 @@ package net.javadiscord.javabot.listener;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateNicknameEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.javadiscord.javabot.Bot;
+import net.javadiscord.javabot.util.GuildUtils;
 import net.javadiscord.javabot.util.StringUtils;
 
 import java.io.IOException;
@@ -38,23 +41,28 @@ public class PingableNameListener extends ListenerAdapter {
 	@Override
 	public void onGuildMemberJoin(GuildMemberJoinEvent event) {
 		if (!isPingable(event.getUser().getName()) && !canBypassCheck(event.getMember())) {
-			String newName = generateRandomName();
-			event.getMember().modifyNickname(newName).queue();
-			event.getUser().openPrivateChannel()
-					.flatMap(channel -> channel.sendMessageFormat("Your nickname has been set to %s since your username's first three characters were deemed as not-pingable.", newName))
-					.queue();
+			changeName(event.getMember(), Bot.config.get(event.getGuild()).getModeration().getLogChannel());
 		}
 	}
 
 	@Override
 	public void onGuildMemberUpdateNickname(GuildMemberUpdateNicknameEvent event) {
 		if (!isPingable(event.getNewNickname()) && !isPingable(event.getUser().getName()) && !canBypassCheck(event.getMember())) {
-			String newName = generateRandomName();
-			event.getMember().modifyNickname(newName).queue();
-			event.getUser().openPrivateChannel()
-					.flatMap(channel -> channel.sendMessageFormat("Your nickname has been set to `%s` since both your user- and nickname's first three characters were deemed as not-pingable.", newName))
-					.queue();
+			changeName(event.getMember(), Bot.config.get(event.getGuild()).getModeration().getLogChannel());
 		}
+	}
+
+	/**
+	 * Changes the given {@link Member}s name to a randomly generated one.
+	 * @param member The Member whose name should be changed.
+	 */
+	private void changeName(Member member, TextChannel logChannel) {
+		String newName = generateRandomName();
+		member.modifyNickname(newName).queue();
+		member.getUser().openPrivateChannel()
+				.flatMap(channel -> channel.sendMessageFormat("Your nickname has been set to `%s` since both your user- and nickname's first three characters were deemed as not-pingable.", newName))
+				.queue();
+		logChannel.sendMessageFormat("Changed %s's nickname to %s.", member.getAsMention(), newName).queue();
 	}
 
 	/**
@@ -79,8 +87,8 @@ public class PingableNameListener extends ListenerAdapter {
 	 * @return The generated name.
 	 */
 	private String generateRandomName() {
-		String noun = this.nouns.get(random.nextInt(nouns.size()));
-		String adjective = this.adjectives.get(random.nextInt(adjectives.size()));
+		String noun = nouns.get(random.nextInt(nouns.size()));
+		String adjective = adjectives.get(random.nextInt(adjectives.size()));
 		int number = random.nextInt(10000);
 		return StringUtils.capitalize(adjective) + StringUtils.capitalize(noun) + number;
 	}
@@ -92,11 +100,11 @@ public class PingableNameListener extends ListenerAdapter {
 	 */
 	private static List<String> readStrings(String url) {
 		List<String> list;
-		try(Scanner scan = new Scanner(new URL(url).openStream()).useDelimiter("\\A")) {
+		try (Scanner scan = new Scanner(new URL(url).openStream()).useDelimiter("\\A")) {
 			String response = scan.next();
 			list = Arrays.stream(response.split("\n")).collect(Collectors.toList());
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error("Error during retrieval of words.");
 			list = new ArrayList<>();
 		}
 
