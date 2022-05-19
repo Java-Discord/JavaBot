b3fa840e630287ab7dfc25b6ffee265ddb88ab9d
liquiddevelopmentnet <info@minemix.de>
2022-05-19 17:55:46 +0200

    Fixed bug where the url parsing just crashes and the suggestion message will be not created.

--- src/main/java/net/javadiscord/javabot/systems/moderation/AutoMod.java
+++ src/main/java/net/javadiscord/javabot/systems/moderation/AutoMod.java
@@ -202,11 +202,10 @@ public class AutoMod extends ListenerAdapter {
 				String url = urlMatcher.group(0).trim();
 				try {
 					URI uri = new URI(url);
-					if (spamUrls.contains(uri.getHost())) {
+					if (uri.getHost() != null && spamUrls.contains(uri.getHost())) {
 						return true;
 					}
-				} catch (URISyntaxException e) {
-					e.printStackTrace();
+				} catch (URISyntaxException ignored) {
 				}
 			}
 		}
