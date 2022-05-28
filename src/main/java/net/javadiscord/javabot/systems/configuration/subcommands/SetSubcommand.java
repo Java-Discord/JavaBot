72fd17b19e84a9b7f24e741eed7cd325f59e28b4
Dynxsty <jasonlessenich@gmail.com>
2022-05-28 19:02:47 +0200

    Added missing annotations

--- src/main/java/net/javadiscord/javabot/systems/configuration/subcommands/SetSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/configuration/subcommands/SetSubcommand.java
@@ -8,12 +8,14 @@ import net.javadiscord.javabot.data.config.GuildConfig;
 import net.javadiscord.javabot.util.Responses;
 import net.javadiscord.javabot.data.config.UnknownPropertyException;
 
+import javax.annotation.Nonnull;
+
 /**
  * Subcommand that allows staff-members to edit the bot's configuration.
  */
 public class SetSubcommand extends ConfigSubcommand {
 	@Override
-	public ReplyCallbackAction handleConfigSubcommand(SlashCommandInteractionEvent event, GuildConfig config) throws UnknownPropertyException {
+	public ReplyCallbackAction handleConfigSubcommand(@Nonnull SlashCommandInteractionEvent event, @Nonnull GuildConfig config) throws UnknownPropertyException {
 		OptionMapping propertyOption = event.getOption("property");
 		OptionMapping valueOption = event.getOption("value");
 		if (propertyOption == null || valueOption == null) {
