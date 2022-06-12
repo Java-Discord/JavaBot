3a5a72b9b27a30cd4e756eed8ce1678746c21048
Dynxsty <jasonlessenich@gmail.com>
2022-06-12 12:20:44 +0200

    increased text progessbar size

--- src/main/java/net/javadiscord/javabot/systems/help/commands/subcommands/HelpAccountSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/help/commands/subcommands/HelpAccountSubcommand.java
@@ -88,7 +88,7 @@ public class HelpAccountSubcommand implements SlashCommand {
 			double percentToGoalXp = (currentXp / goalXp) * 100.0;
 			sb.append(String.format("%.0f / %.0f XP (%.2f%%) until %s\n", currentXp, goalXp, percentToGoalXp, nextRoleAndXp.first().getAsMention()))
 					.append(String.format("%.0f Total XP\n", account.getExperience()))
-					.append(StringUtils.buildTextProgressBar(percentToGoalXp / 100.0, 12));
+					.append(StringUtils.buildTextProgressBar(percentToGoalXp / 100.0, 20));
 		} else {
 			sb.append(String.format("%.0f Total XP (MAX. LEVEL)", account.getExperience()));
 		}
