5fe1d32558c5e946a09ed89b4825fc3b21dec0cd
danthe1st <daniel@wwwmaster.at>
2022-06-17 18:34:51 +0200

    use club.minnced:discord-webhooks for replacing fucks with hugs

--- src/main/java/net/javadiscord/javabot/listener/HugListener.java
+++ src/main/java/net/javadiscord/javabot/listener/HugListener.java
@@ -1,24 +1,23 @@
 package net.javadiscord.javabot.listener;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import javax.annotation.Nonnull;
 
-import net.dv8tion.jda.api.entities.BaseGuildMessageChannel;
+import club.minnced.discord.webhook.WebhookClientBuilder;
+import club.minnced.discord.webhook.external.JDAWebhookClient;
+import club.minnced.discord.webhook.send.AllowedMentions;
+import club.minnced.discord.webhook.send.WebhookMessageBuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.GuildMessageChannel;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.Webhook;
-import net.dv8tion.jda.api.entities.WebhookType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.utils.AttachmentOption;
-import net.dv8tion.jda.internal.entities.WebhookImpl;
-import net.dv8tion.jda.internal.requests.restaction.WebhookMessageActionImpl;
 import net.javadiscord.javabot.Bot;
 
 /**
@@ -33,27 +32,31 @@ public class HugListener extends ListenerAdapter {
 		if (Bot.autoMod.hasSuspiciousLink(event.getMessage()) || Bot.autoMod.hasAdvertisingLink(event.getMessage())) {
 			return;
 		}
-		TextChannel tc=event.getTextChannel();
+		TextChannel tc = null;
+		if(event.isFromType(ChannelType.TEXT)) {
+			tc = event.getTextChannel();
+		}
 		if(event.isFromThread()) {
 			GuildMessageChannel parentChannel = event.getThreadChannel().getParentMessageChannel();
 			if(parentChannel instanceof TextChannel textChannel) {
-				tc=textChannel;
+				tc = textChannel;
 			}
 		}
-		if(tc==null) {
+		if(tc == null) {
 			return;
 		}
-		final TextChannel textChannel=tc;
-		String content=event.getMessage().getContentRaw();
-		String lowerCaseContent=content.toLowerCase();
+		final TextChannel textChannel = tc;
+		String content = event.getMessage().getContentRaw();
+		String lowerCaseContent = content.toLowerCase();
 		if(lowerCaseContent.contains("fuck")) {
-			StringBuilder sb=new StringBuilder(content.length());
-			int index=0;
-			int indexBkp=index;
-			while((index=lowerCaseContent.indexOf("fuck",index))!=-1) {
+			long threadId = event.isFromThread()?event.getThreadChannel().getIdLong():0;
+			StringBuilder sb = new StringBuilder(content.length());
+			int index = 0;
+			int indexBkp = index;
+			while((index = lowerCaseContent.indexOf("fuck",index)) != -1) {
 				sb.append(content.substring(indexBkp,index));
 				sb.append("hug");
-				indexBkp=index+++4;
+				indexBkp=index++ +4;
 			}
 			
 			sb.append(content.substring(indexBkp,content.length()));
@@ -61,16 +64,16 @@ public class HugListener extends ListenerAdapter {
 				textChannel.retrieveWebhooks().queue(webhooks->{
 					Optional<Webhook> hook = webhooks
 					.stream()
-					.filter(webhook->webhook.getChannel().getIdLong()==event.getChannel().getIdLong())
+					.filter(webhook->webhook.getChannel().getIdLong() == event.getChannel().getIdLong())
 					.filter(wh->wh.getToken()!=null)
 					.findAny();
 					if(hook.isPresent()) {
-						buildMessage(hook.get(), event.getMessage(), sb.toString(),(BaseGuildMessageChannel) event.getGuildChannel());
+						sendWebhookMessage(hook.get(), event.getMessage(), sb.toString(),threadId);
 					}else {
 						textChannel
 							.createWebhook("JavaBot-hug")
 							.queue(wh->
-								buildMessage(wh, event.getMessage(), sb.toString(),(BaseGuildMessageChannel) event.getGuildChannel())
+								sendWebhookMessage(wh, event.getMessage(), sb.toString(), threadId)
 							);
 					}
 				});
@@ -78,31 +81,32 @@ public class HugListener extends ListenerAdapter {
 		}
 	}
 	
-	private void buildMessage(Webhook webhook, Message originalMessage, String newMessageContent, BaseGuildMessageChannel channel){
-		WebhookImpl webhookImpl = new WebhookImpl(channel, webhook.getJDA(), webhook.getIdLong(), WebhookType.INCOMING);
-		webhookImpl.setToken(webhook.getToken());
-		WebhookMessageActionImpl<Void> ret = webhookImpl
-				.sendMessage(newMessageContent)
-				.allowedMentions(Collections.emptyList());
+	private void sendWebhookMessage(Webhook webhook, Message originalMessage, String newMessageContent, long threadId){
+		JDAWebhookClient client = new WebhookClientBuilder(webhook.getIdLong(), webhook.getToken())
+				.setThreadId(threadId)
+				.buildJDA();
+		WebhookMessageBuilder message = new WebhookMessageBuilder()
+				.setContent(newMessageContent)
+				.setAllowedMentions(AllowedMentions.none())
+				.setAvatarUrl(originalMessage.getMember().getEffectiveAvatarUrl())
+				.setUsername(originalMessage.getMember().getEffectiveName());
 		
 		List<Attachment> attachments = originalMessage.getAttachments();
 		@SuppressWarnings("unchecked")
-		CompletableFuture<?>[] futures=new CompletableFuture<?>[attachments.size()];
-		
+		CompletableFuture<?>[] futures = new CompletableFuture<?>[attachments.size()];
 		for(int i = 0; i < attachments.size(); i++){
 			Attachment attachment = attachments.get(i);
-			futures[i]=attachment
+			futures[i] = attachment
 					.getProxy()
 					.download()
-					.thenAccept(is->
-						ret.addFile(
-								is,
-								attachment.getFileName(),
-								attachment.isSpoiler() ? new AttachmentOption[] { AttachmentOption.SPOILER } : new AttachmentOption[0]
+					.thenAccept(is ->
+						message.addFile(
+								(attachment.isSpoiler()?"SPOILER_":"")+attachment.getFileName(),
+								is
 						)
 					);
 		}
 		CompletableFuture.allOf(futures)
-			.thenAccept(unused->ret.queue());
+			.thenAccept(unused->client.send(message.build()));
 	}
 }
