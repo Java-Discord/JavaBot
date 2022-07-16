00347544bec57cf9e99035ca2182199fa3971030
Dynxsty <jasonlessenich@gmail.com>
2022-07-16 13:20:07 +0200

    Removed all Jam Commands

--- src/main/java/net/javadiscord/javabot/systems/jam/model/JamSubmissionVote.java
+++
@@ -1,7 +0,0 @@
-package net.javadiscord.javabot.systems.jam.model;
-
-/**
- * Simple data class that represents a single Jam Submission Vote.
- */
-public class JamSubmissionVote {
-}
--- src/main/java/net/javadiscord/javabot/systems/jam/model/JamThemeVote.java
+++
@@ -1,7 +0,0 @@
-package net.javadiscord.javabot.systems.jam.model;
-
-/**
- * Simple data class that represents a single Jam Theme Vote.
- */
-public class JamThemeVote {
-}
