18025ab0eef8f2be9d4197ab66d065ce7153991c
Dynxsty <jasonlessenich@gmail.com>
2022-07-25 17:28:55 +0200

    Removed overrides

--- src/main/java/net/javadiscord/javabot/systems/configuration/ConfigCommand.java
+++ src/main/java/net/javadiscord/javabot/systems/configuration/ConfigCommand.java
@@ -1,7 +1,6 @@
 package net.javadiscord.javabot.systems.configuration;
 
 import com.dynxsty.dih4jda.interactions.commands.SlashCommand;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.javadiscord.javabot.Bot;
 import net.javadiscord.javabot.systems.configuration.subcommands.ExportConfigSubcommand;
@@ -18,10 +17,7 @@ public class ConfigCommand extends SlashCommand implements CommandModerationPerm
 	 * The constructor of this class, which sets the corresponding {@link net.dv8tion.jda.api.interactions.commands.build.SlashCommandData}.
 	 */
 	public ConfigCommand() {
-		setModerationSlashCommandData(Commands.slash("config", "Administrative Commands for managing the bot's configuration.")
-				.setDefaultPermissions(DefaultMemberPermissions.DISABLED)
-				.setGuildOnly(true)
-		);
+		setModerationSlashCommandData(Commands.slash("config", "Administrative Commands for managing the bot's configuration."));
 		addSubcommands(new ExportConfigSubcommand(), new GetConfigSubcommand(), new SetConfigSubcommand());
 		requireUsers(Bot.config.getSystems().getAdminConfig().getAdminUsers());
 	}
