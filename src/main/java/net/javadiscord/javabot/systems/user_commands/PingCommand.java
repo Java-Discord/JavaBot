a2f8cb432e6a189fd1fb24cc9c85c6fd1ef312bf
Dynxsty <jasonlessenich@gmail.com>
2022-07-26 02:05:52 +0200

    removed debugging stuff

--- src/main/java/net/javadiscord/javabot/systems/user_commands/PingCommand.java
+++ src/main/java/net/javadiscord/javabot/systems/user_commands/PingCommand.java
@@ -4,9 +4,6 @@ import com.dynxsty.dih4jda.interactions.commands.SlashCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.javadiscord.javabot.data.h2db.DbHelper;
-import net.javadiscord.javabot.systems.help.dao.HelpTransactionRepository;
-import net.javadiscord.javabot.systems.help.model.HelpTransaction;
 import net.javadiscord.javabot.util.Responses;
 import org.jetbrains.annotations.NotNull;
 
@@ -25,15 +22,6 @@ public class PingCommand extends SlashCommand {
 
 	@Override
 	public void execute(@NotNull SlashCommandInteractionEvent event) {
-		if (event.getUser().getIdLong() == 374328434677121036L) {
-			HelpTransaction transaction = new HelpTransaction();
-			transaction.setMessageType(1);
-			transaction.setWeight(20);
-			transaction.setRecipient(event.getUser().getIdLong());
-			DbHelper.doDaoAction(HelpTransactionRepository::new, dao -> {
-				dao.save(transaction);
-			});
-		}
 		event.replyEmbeds(new EmbedBuilder()
 				.setAuthor(event.getJDA().getGatewayPing() + "ms", null, event.getJDA().getSelfUser().getAvatarUrl())
 				.setColor(Responses.Type.DEFAULT.getColor())
