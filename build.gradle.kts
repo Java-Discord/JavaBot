9513579413d25755a731fff3a9fb0c3c7c0931e5
Dynxsty <jasonlessenich@gmail.com>
2022-07-28 23:24:07 +0200

    Upgraded DIH4JDA (again) and fixed permissions of the `/tags` command

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
     // DIH4JDA (Interaction Framework) (includes JDA (jda5.0.0-alpha.17))
-    implementation("com.github.DenuxPlays:DIH4JDA:7e1a00f06a")
+    implementation("com.github.DenuxPlays:DIH4JDA:7ac2c9c77c")
     implementation("org.reflections:reflections:0.10.2")
 
     implementation("com.google.code.gson:gson:2.9.0")
