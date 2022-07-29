520c84d64d73699a18387b3d4f50a8c1eb16f8e2
Dynxsty <jasonlessenich@gmail.com>
2022-07-29 12:03:37 +0200

    Fixed Message Cache Primary Key Violation

--- src/main/java/net/javadiscord/javabot/data/h2db/message_cache/dao/MessageCacheRepository.java
+++ src/main/java/net/javadiscord/javabot/data/h2db/message_cache/dao/MessageCacheRepository.java
@@ -2,6 +2,7 @@ package net.javadiscord.javabot.data.h2db.message_cache.dao;
 
 import lombok.RequiredArgsConstructor;
 import net.javadiscord.javabot.data.h2db.message_cache.model.CachedMessage;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -39,11 +40,12 @@ public class MessageCacheRepository {
 	 * @param messages The List to insert.
 	 * @throws SQLException If an error occurs.
 	 */
-	public void insertList(List<CachedMessage> messages) throws SQLException {
+	public void insertList(@NotNull List<CachedMessage> messages) throws SQLException {
+		List<CachedMessage> all = getAll();
 		try (PreparedStatement stmt = con.prepareStatement("INSERT INTO message_cache (message_id, author_id, message_content) VALUES (?, ?, ?)",
 				Statement.RETURN_GENERATED_KEYS
 		)) {
-			for (CachedMessage msg : messages) {
+			for (CachedMessage msg : messages.stream().filter(s -> !all.contains(s)).toList()) {
 				stmt.setLong(1, msg.getMessageId());
 				stmt.setLong(2, msg.getAuthorId());
 				stmt.setString(3, msg.getMessageContent());
@@ -59,7 +61,7 @@ public class MessageCacheRepository {
 	 * @return Whether there were rows affected by this process.
 	 * @throws SQLException If an error occurs.
 	 */
-	public boolean update(CachedMessage message) throws SQLException {
+	public boolean update(@NotNull CachedMessage message) throws SQLException {
 		try (PreparedStatement stmt = con.prepareStatement("UPDATE message_cache SET message_content = ? WHERE message_id = ?",
 				Statement.RETURN_GENERATED_KEYS
 		)) {
