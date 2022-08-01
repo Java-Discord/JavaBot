fdfd385acddc23107772f49d5c7310325d9c1e92
Dynxsty <jasonlessenich@gmail.com>
2022-08-01 11:38:54 +0200

    Made `toChoice` private

--- src/main/java/net/javadiscord/javabot/systems/user_preferences/commands/PreferencesSetSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/user_preferences/commands/PreferencesSetSubcommand.java
@@ -50,7 +50,7 @@ public class PreferencesSetSubcommand extends SlashCommand.Subcommand {
 		}
 	}
 
-	public Command.Choice toChoice(@NotNull Preference preference) {
+	private Command.Choice toChoice(@NotNull Preference preference) {
 		return new Command.Choice(preference.toString(), String.valueOf(preference.ordinal()));
 	}
 }
