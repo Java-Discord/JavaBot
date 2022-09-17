79a54c835ef016e6154822186cd52d733be053b7
tharun634 <tharun634@gmail.com>
2022-09-17 12:21:03 +0530

    fix checkstyle error

--- src/main/java/net/javadiscord/javabot/systems/help/commands/CloseCommand.java
+++ src/main/java/net/javadiscord/javabot/systems/help/commands/CloseCommand.java
@@ -9,14 +9,14 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
  */
 public class CloseCommand extends UnreserveCommand {
 
-  /**
-   * The constructor of this class, which sets the corresponding
-   * {@link net.dv8tion.jda.api.interactions.commands.build.SlashCommandData}.
-   */
-  public CloseCommand() {
-    setSlashCommandData(
-        Commands.slash("close", "Unreserves this help channel so that others can use it.")
-            .setGuildOnly(true).addOption(OptionType.STRING, "reason",
-                "The reason why you're unreserving this channel", false));
-  }
+	/**
+	 * The constructor of this class, which sets the corresponding
+	 * {@link net.dv8tion.jda.api.interactions.commands.build.SlashCommandData}.
+	 */
+	public CloseCommand() {
+		setSlashCommandData(
+				Commands.slash("close", "Unreserves this help channel so that others can use it.")
+						.setGuildOnly(true).addOption(OptionType.STRING, "reason",
+								"The reason why you're unreserving this channel", false));
+	}
 }
