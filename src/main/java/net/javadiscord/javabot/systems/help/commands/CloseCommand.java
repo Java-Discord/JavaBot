2ad7e59f3b53fd083b1257107313f2fcf157ca3a
tharun634 <tharun634@gmail.com>
2022-09-17 12:10:30 +0530

    Added close alias

---
+++ src/main/java/net/javadiscord/javabot/systems/help/commands/CloseCommand.java
@@ -0,0 +1,22 @@
+package net.javadiscord.javabot.systems.help.commands;
+
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+/**
+ * A simple command that can be used inside reserved help channels to immediately unreserve them,
+ * instead of waiting for a timeout.
+ */
+public class CloseCommand extends UnreserveCommand {
+
+  /**
+   * The constructor of this class, which sets the corresponding
+   * {@link net.dv8tion.jda.api.interactions.commands.build.SlashCommandData}.
+   */
+  public CloseCommand() {
+    setSlashCommandData(
+        Commands.slash("close", "Unreserves this help channel so that others can use it.")
+            .setGuildOnly(true).addOption(OptionType.STRING, "reason",
+                "The reason why you're unreserving this channel", false));
+  }
+}
