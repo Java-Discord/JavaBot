0b105829501d71e21e2da72c5647d224ac71aefa
Dynxsty <jasonlessenich@gmail.com>
2022-12-11 23:08:00 +0100

    Swapped staff-role with qotw-role

--- src/main/java/net/javadiscord/javabot/systems/qotw/QOTWReminderJob.java
+++ src/main/java/net/javadiscord/javabot/systems/qotw/QOTWReminderJob.java
@@ -3,7 +3,7 @@ package net.javadiscord.javabot.systems.qotw;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.javadiscord.javabot.data.config.BotConfig;
-import net.javadiscord.javabot.data.config.guild.ModerationConfig;
+import net.javadiscord.javabot.data.config.guild.QOTWConfig;
 import net.javadiscord.javabot.systems.notification.NotificationService;
 import net.javadiscord.javabot.systems.qotw.dao.QuestionQueueRepository;
 import net.javadiscord.javabot.systems.qotw.model.QOTWQuestion;
@@ -15,8 +15,6 @@ import lombok.RequiredArgsConstructor;
 import java.sql.SQLException;
 import java.util.Optional;
 
-import javax.sql.DataSource;
-
 /**
  * Checks that there's a question in the QOTW queue ready for posting soon.
  */
@@ -26,7 +24,6 @@ public class QOTWReminderJob {
 	private final JDA jda;
 	private final NotificationService notificationService;
 	private final BotConfig botConfig;
-	private final DataSource dataSource;
 	private final QuestionQueueRepository questionQueueRepository;
 
 	/**
@@ -36,12 +33,12 @@ public class QOTWReminderJob {
 	@Scheduled(cron = "0 0 9 * * *") // Daily, 09:00 UTC
 	public void execute() throws SQLException {
 		for (Guild guild : jda.getGuilds()) {
-			ModerationConfig config = botConfig.get(guild).getModerationConfig();
+			QOTWConfig config = botConfig.get(guild).getQotwConfig();
 			Optional<QOTWQuestion> q = questionQueueRepository.getNextQuestion(guild.getIdLong());
 			if (q.isEmpty()) {
 				notificationService.withGuild(guild).sendToModerationLog(m -> m.sendMessageFormat(
 						"Warning! %s There's no Question of the Week in the queue. Please add one before it's time to post!",
-						config.getStaffRole().getAsMention()));
+						config.getQOTWReviewRole().getAsMention()));
 			}
 		}
 	}
