827424e23a614dd89f7fb33da5b7f55b1d9186fa
Dynxsty <jasonlessenich@gmail.com>
2022-12-12 19:05:50 +0100

    Removed transactions from `/help account`

--- src/main/java/net/javadiscord/javabot/systems/help/commands/HelpAccountSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/help/commands/HelpAccountSubcommand.java
@@ -1,6 +1,5 @@
 package net.javadiscord.javabot.systems.help.commands;
 
-import xyz.dynxsty.dih4jda.interactions.commands.application.SlashCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
@@ -13,13 +12,13 @@ import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import net.javadiscord.javabot.data.h2db.DbActions;
 import net.javadiscord.javabot.systems.help.HelpExperienceService;
 import net.javadiscord.javabot.systems.help.model.HelpAccount;
-import net.javadiscord.javabot.systems.help.model.HelpTransaction;
 import net.javadiscord.javabot.util.ExceptionLogger;
 import net.javadiscord.javabot.util.Pair;
 import net.javadiscord.javabot.util.Responses;
 import net.javadiscord.javabot.util.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.dao.DataAccessException;
+import xyz.dynxsty.dih4jda.interactions.commands.application.SlashCommand;
 
 /**
  * <h3>This class represents the /help account command.</h3>
@@ -33,7 +32,8 @@ public class HelpAccountSubcommand extends SlashCommand.Subcommand {
 
 	/**
 	 * The constructor of this class, which sets the corresponding {@link SubcommandData}.
-	 * @param dbActions  An object responsible for various database actions
+	 *
+	 * @param dbActions             An object responsible for various database actions
 	 * @param helpExperienceService Service object that handles Help Experience Transactions.
 	 */
 	public HelpAccountSubcommand(DbActions dbActions, HelpExperienceService helpExperienceService) {
@@ -41,14 +41,12 @@ public class HelpAccountSubcommand extends SlashCommand.Subcommand {
 		this.helpExperienceService = helpExperienceService;
 		setCommandData(new SubcommandData("account", "Shows an overview of your Help Account.")
 				.addOption(OptionType.USER, "user", "If set, show the Help Account of the specified user instead.", false)
-				.addOption(OptionType.BOOLEAN, "show-transactions", "Should the recent transactions be shown?", false)
 		);
 	}
 
 	@Override
 	public void execute(@NotNull SlashCommandInteractionEvent event) {
 		User user = event.getOption("user", event::getUser, OptionMapping::getAsUser);
-		boolean showTransactions = event.getOption("show-transactions", false, OptionMapping::getAsBoolean);
 		long totalThanks = dbActions.count(
 				"SELECT COUNT(id) FROM help_channel_thanks WHERE helper_id = ?",
 				s -> s.setLong(1, user.getIdLong())
@@ -59,40 +57,26 @@ public class HelpAccountSubcommand extends SlashCommand.Subcommand {
 		);
 		try {
 			HelpAccount account = helpExperienceService.getOrCreateAccount(user.getIdLong());
-			event.replyEmbeds(buildHelpAccountEmbed(account, user, event.getGuild(), totalThanks, weekThanks, showTransactions)).queue();
+			event.replyEmbeds(buildHelpAccountEmbed(account, user, event.getGuild(), totalThanks, weekThanks)).queue();
 		} catch (DataAccessException e) {
 			ExceptionLogger.capture(e, getClass().getSimpleName());
 			Responses.error(event, e.getMessage()).queue();
 		}
 	}
 
-	private @NotNull MessageEmbed buildHelpAccountEmbed(HelpAccount account, @NotNull User user, Guild guild, long totalThanks, long weekThanks, boolean showTransactions) {
-		return  new EmbedBuilder()
+	private @NotNull MessageEmbed buildHelpAccountEmbed(HelpAccount account, @NotNull User user, Guild guild, long totalThanks, long weekThanks) {
+		return new EmbedBuilder()
 				.setAuthor(user.getAsTag(), null, user.getEffectiveAvatarUrl())
 				.setTitle("Help Account")
 				.setThumbnail(user.getEffectiveAvatarUrl())
 				.setDescription("Here are some statistics about how you've helped others here.")
-				.addField("Experience (BETA)", String.format("%s%s",
-					formatExperience(guild, account),
-					showTransactions ? String.format("\n\n**Recent Transactions**\n```diff\n%s```", formatTransactionHistory(user.getIdLong())) : ""), false)
+				.addField("Experience (BETA)", formatExperience(guild, account), false)
 				.addField("Total Times Thanked", String.format("**%s**", totalThanks), true)
 				.addField("Times Thanked This Week", String.format("**%s**", weekThanks), true)
 				.build();
 	}
 
-	private @NotNull String formatTransactionHistory(long userId) {
-		StringBuilder sb = new StringBuilder();
-		try {
-			for (HelpTransaction t :helpExperienceService.getRecentTransactions(userId, 3)) {
-				sb.append(t.format()).append("\n\n");
-			}
-		} catch (DataAccessException e) {
-			ExceptionLogger.capture(e, getClass().getSimpleName());
-		}
-		return sb.toString().length() > 0 ? sb.toString() : "No recent transactions";
-	}
-
-	private String formatExperience(Guild guild, HelpAccount account) {
+	private @NotNull String formatExperience(Guild guild, @NotNull HelpAccount account) {
 		Pair<Role, Double> previousRoleAndXp = account.getPreviousExperienceGoal(guild);
 		Pair<Role, Double> currentRoleAndXp = account.getCurrentExperienceGoal(guild);
 		Pair<Role, Double> nextRoleAndXp = account.getNextExperienceGoal(guild);
@@ -102,13 +86,13 @@ public class HelpAccountSubcommand extends SlashCommand.Subcommand {
 
 		if (currentRoleAndXp.first() != null) {
 			// Show the current experience level on the first line.
-			sb.append(String.format("%s\n", currentRoleAndXp.first().getAsMention()));
+			sb.append(String.format("%s%n", currentRoleAndXp.first().getAsMention()));
 		}
 		// Below, show the progress to the next level, or just the XP if they've reached the max level.
 		if (goalXp > 0) {
 			double percentToGoalXp = (currentXp / goalXp) * 100.0;
-			sb.append(String.format("%.0f / %.0f XP (%.2f%%) until %s\n", currentXp, goalXp, percentToGoalXp, nextRoleAndXp.first().getAsMention()))
-					.append(String.format("%.0f Total XP\n", account.getExperience()))
+			sb.append(String.format("%.0f / %.0f XP (%.2f%%) until %s%n", currentXp, goalXp, percentToGoalXp, nextRoleAndXp.first().getAsMention()))
+					.append(String.format("%.0f Total XP%n", account.getExperience()))
 					.append(StringUtils.buildTextProgressBar(percentToGoalXp / 100.0, 20));
 		} else {
 			sb.append(String.format("%.0f Total XP (MAX. LEVEL)", account.getExperience()));
