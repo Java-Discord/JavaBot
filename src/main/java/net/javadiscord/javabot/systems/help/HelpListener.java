ce08953dc7f9b386fa536d6281e8fff38138c256
Dynxsty <jasonlessenich@gmail.com>
2022-12-17 17:29:54 +0100

    Fixed thanks-closing & fixed rare race condition

--- src/main/java/net/javadiscord/javabot/systems/help/HelpListener.java
+++ src/main/java/net/javadiscord/javabot/systems/help/HelpListener.java
@@ -22,7 +22,6 @@ import net.javadiscord.javabot.data.config.guild.HelpConfig;
 import net.javadiscord.javabot.data.h2db.DbActions;
 import net.javadiscord.javabot.systems.help.dao.HelpAccountRepository;
 import net.javadiscord.javabot.systems.help.dao.HelpTransactionRepository;
-import net.javadiscord.javabot.systems.user_preferences.UserPreferenceService;
 import net.javadiscord.javabot.util.ExceptionLogger;
 import net.javadiscord.javabot.util.Responses;
 import org.jetbrains.annotations.NotNull;
@@ -32,11 +31,13 @@ import xyz.dynxsty.dih4jda.util.ComponentIdBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
- * Listens for all events releated to the forum help channel system.
+ * Listens for all events related to the forum help channel system.
  */
 @RequiredArgsConstructor
 @AutoDetectableComponentHandler({HelpManager.HELP_THANKS_IDENTIFIER, HelpManager.HELP_CLOSE_IDENTIFIER, HelpManager.HELP_GUIDELINES_IDENTIFIER})
@@ -45,16 +46,33 @@ public class HelpListener extends ListenerAdapter implements ButtonHandler {
 	/**
 	 * A static Map that holds all messages that was sent in a specific reserved forum channel.
 	 */
-	public static final Map<Long, List<Message>> HELP_POST_MESSAGES = new HashMap<>();
+	protected static final Map<Long, List<Message>> HELP_POST_MESSAGES = new HashMap<>();
+	private static final Set<Long> newPosts;
+
+	static {
+		newPosts = new HashSet<>();
+	}
 
 	private final BotConfig botConfig;
 	private final HelpAccountRepository helpAccountRepository;
 	private final HelpTransactionRepository helpTransactionRepository;
-	private final UserPreferenceService userPreferenceService;
 	private final DbActions dbActions;
 
 	@Override
 	public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+		// listen for the posts initial message. Why this has to be done is further described in
+		// https://canary.discord.com/channels/125227483518861312/1053705384466059354/1053705384466059354
+		if (newPosts.contains(event.getChannel().getIdLong())) {
+			HelpConfig config = botConfig.get(event.getGuild()).getHelpConfig();
+			ThreadChannel post = event.getChannel().asThreadChannel();
+			// send post buttons
+			post.sendMessageComponents(ActionRow.of(
+					Button.primary(ComponentIdBuilder.build(HelpManager.HELP_CLOSE_IDENTIFIER, post.getIdLong()), "Close Post"),
+					Button.secondary(ComponentIdBuilder.build(HelpManager.HELP_GUIDELINES_IDENTIFIER), "View Help Guidelines")
+			)).queue(success -> post.sendMessageFormat(config.getReservedChannelMessageTemplate(), UserSnowflake.fromId(post.getOwnerId()).getAsMention(), config.getInactivityTimeoutMinutes()).queue());
+			newPosts.remove(event.getChannel().getIdLong());
+			return;
+		}
 		if (event.getMessage().getAuthor().isSystem() || event.getMessage().getAuthor().isBot()) {
 			return;
 		}
@@ -85,11 +103,9 @@ public class HelpListener extends ListenerAdapter implements ButtonHandler {
 		if (isInvalidHelpForumChannel(post.getParentChannel().asForumChannel())) {
 			return;
 		}
-		// send post buttons
-		post.sendMessageComponents(ActionRow.of(
-				Button.primary(ComponentIdBuilder.build(HelpManager.HELP_CLOSE_IDENTIFIER, post.getIdLong()), "Close Post"),
-				Button.secondary(ComponentIdBuilder.build(HelpManager.HELP_GUIDELINES_IDENTIFIER), "View Help Guidelines")
-		)).queue(success -> post.sendMessageFormat(config.getReservedChannelMessageTemplate(), UserSnowflake.fromId(post.getOwnerId()).getAsMention(), config.getInactivityTimeoutMinutes()).queue());
+		// add thread id to a temporary cache to avoid potential missing author message
+		// more info on why this has to be done: https://canary.discord.com/channels/125227483518861312/1053705384466059354/1053705384466059354
+		newPosts.add(event.getChannel().getIdLong());
 	}
 
 	@Override
@@ -105,8 +121,10 @@ public class HelpListener extends ListenerAdapter implements ButtonHandler {
 		HelpManager manager = new HelpManager(post, dbActions, botConfig, helpAccountRepository, helpTransactionRepository);
 		switch (id[0]) {
 			case HelpManager.HELP_THANKS_IDENTIFIER -> handleHelpThanksInteraction(event, manager, id);
-			case HelpManager.HELP_GUIDELINES_IDENTIFIER -> handleReplyGuidelines(event, post.getParentChannel().asForumChannel());
+			case HelpManager.HELP_GUIDELINES_IDENTIFIER ->
+					handleReplyGuidelines(event, post.getParentChannel().asForumChannel());
 			case HelpManager.HELP_CLOSE_IDENTIFIER -> handlePostClose(event, manager);
+			default -> event.reply("Unknown interaction.").queue();
 		}
 	}
 
@@ -128,32 +146,44 @@ public class HelpListener extends ListenerAdapter implements ButtonHandler {
 			return;
 		}
 		switch (id[2]) {
-			case "done" -> {
-				List<Button> buttons = event.getMessage().getButtons();
-				// immediately delete the message
-				event.getMessage().delete().queue(s -> {
-					// close post
-					manager.close(event, false, null);
-					// add experience
-					try {
-						HelpExperienceService service = new HelpExperienceService(botConfig, helpAccountRepository, helpTransactionRepository);
-						Map<Long, Double> experience = HelpManager.calculateExperience(HELP_POST_MESSAGES.get(post.getIdLong()), post.getOwnerIdLong(), config);
-						for (Map.Entry<Long, Double> entry : experience.entrySet()) {
-							service.performTransaction(entry.getKey(), entry.getValue(), config.getGuild());
-						}
-					} catch (DataAccessException e) {
-						ExceptionLogger.capture(e, getClass().getName());
-					}
-					// thank all helpers
-					buttons.stream().filter(ActionComponent::isDisabled)
-							.filter(b -> b.getId() != null)
-							.forEach(b -> manager.thankHelper(event, post, Long.parseLong(ComponentIdBuilder.split(b.getId())[2])));
-				});
-
-			}
+			case "done" -> handleThanksCloseButton(event, manager, post, config);
 			case "cancel" -> event.getMessage().delete().queue();
-			default -> event.editButton(event.getButton().asDisabled()).queue();
+			default -> {
+				List<Button> thankButtons = event.getMessage().getButtons().stream()
+						.filter(b -> b.getId() != null &&
+								!ComponentIdBuilder.split(b.getId())[2].equals("done") &&
+								!ComponentIdBuilder.split(b.getId())[2].equals("cancel")
+						).toList();
+				if (thankButtons.stream().filter(Button::isDisabled).count() == thankButtons.size() - 1) {
+					handleThanksCloseButton(event, manager, post, config);
+				} else {
+					event.editButton(event.getButton().asDisabled()).queue();
+				}
+			}
 		}
+	}
+
+	private void handleThanksCloseButton(@NotNull ButtonInteractionEvent event, HelpManager manager, ThreadChannel post, HelpConfig config) {
+		List<Button> buttons = event.getMessage().getButtons();
+		// immediately delete the message
+		event.getMessage().delete().queue(s -> {
+			// close post
+			manager.close(event, false, null);
+			// add experience
+			try {
+				HelpExperienceService service = new HelpExperienceService(botConfig, helpAccountRepository, helpTransactionRepository);
+				Map<Long, Double> experience = HelpManager.calculateExperience(HELP_POST_MESSAGES.get(post.getIdLong()), post.getOwnerIdLong(), config);
+				for (Map.Entry<Long, Double> entry : experience.entrySet()) {
+					service.performTransaction(entry.getKey(), entry.getValue(), config.getGuild());
+				}
+			} catch (DataAccessException e) {
+				ExceptionLogger.capture(e, getClass().getName());
+			}
+			// thank all helpers
+			buttons.stream().filter(ActionComponent::isDisabled)
+					.filter(b -> b.getId() != null)
+					.forEach(b -> manager.thankHelper(event, post, Long.parseLong(ComponentIdBuilder.split(b.getId())[2])));
+		});
 	}
 
 	private void handleReplyGuidelines(@NotNull IReplyCallback callback, @NotNull ForumChannel channel) {
