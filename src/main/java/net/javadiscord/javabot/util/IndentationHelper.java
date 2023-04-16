378a11393a843ff4bf7f8e5c6d742f372abf2dc6
Fischstaebchen <fisch@git.com>
2023-04-16 05:20:02 +0200

    Changed as per recommendations by danthe1st.

--- src/main/java/net/javadiscord/javabot/util/IndentationHelper.java
+++ src/main/java/net/javadiscord/javabot/util/IndentationHelper.java
@@ -4,176 +4,188 @@ package net.javadiscord.javabot.util;
  * Utility class to help indent strings that contain code.
  */
 public class IndentationHelper {
-    /**
-     * Enum which denotes the different types of indentation possible.
-     * When {@link #NULL} is passed to any method in this class, the output of said method should match the input.
-     */
-    public enum IndentationType {
-        /**
-         * A tab character (\t) should be used for indentation.
-         */
-        TABS("\t"),
-        /**
-         * Four spaces should be used for indentation.
-         */
-        FOUR_SPACES("    "),
-        /**
-         * Two spaces should be used for indentation
-         */
-        TWO_SPACES("  "),
-        /**
-         * Doesn't define an indentation type but
-         * Is used as a substitute to null to indicate that a String given to any method in {@link IndentationHelper} should not be changed.
-         */
-        NULL("");
+	/**
+	 * Enum which denotes the different types of indentation possible.
+	 * When {@link #NULL} is passed to any method in this class, the output of said method should match the input.
+	 */
+	public enum IndentationType {
+		/**
+		 * A tab character (\t) should be used for indentation.
+		 */
+		TABS("\t"),
+		/**
+		 * Four spaces should be used for indentation.
+		 */
+		FOUR_SPACES("    "),
+		/**
+		 * Two spaces should be used for indentation.
+		 */
+		TWO_SPACES("  "),
+		/**
+		 * Doesn't define an indentation type but
+		 * Is used as a substitute to null to indicate that a String given to any method in {@link IndentationHelper} should not be changed.
+		 */
+		NULL("");
 
-        /**
-         * Holds the characters used for indentation of the type.
-         */
-        private final String pattern;
+		/**
+		 * Holds the characters used for indentation of the type.
+		 */
+		private final String pattern;
 
-        /**
-         * Constructs the indentation type
-         * @param pattern The pattern to be used as indentation
-         */
-        IndentationType(String pattern) {
-            this.pattern = pattern;
-        }
+		/**
+		 * Constructs the indentation type.
+		 *
+		 * @param pattern The pattern to be used as indentation
+		 */
+		IndentationType(String pattern) {
+			this.pattern = pattern;
+		}
 
-        /**
-         * Get the pattern for a given Indentation type.
-         * @return the pattern to be used for indenting.
-         */
-        public String getPattern() {
-            return pattern;
-        }
+		/**
+		 * Get the pattern for a given Indentation type.
+		 *
+		 * @return the pattern to be used for indenting.
+		 */
+		public String getPattern() {
+			return pattern;
+		}
 
-        /**
-         * Returns the number of characters a pattern is using.
-         * @return the number of characters the pattern of this type consists of.
-         */
-        private int getNumberOfChars() {
-            return pattern.length();
-        }
+		/**
+		 * Returns the number of characters a pattern is using.
+		 *
+		 * @return the number of characters the pattern of this type consists of.
+		 */
+		private int getNumberOfChars() {
+			return pattern.length();
+		}
+	}
 
-    }
+	/**
+	 * Private enum to denote the current State of the Indentation Process.
+	 */
+	private enum IndentationState {
+		/**
+		 * Denotes that the Process is currently in a codeblock and should indent the code accordingly.
+		 */
+		CODE,
+		/**
+		 * Denotes that the Process is currently in a String literal.
+		 */
+		STRING,
+		/**
+		 * Denotes that the Indentation Process is currently in a character literal.
+		 */
+		CHARACTER,
+		/**
+		 * Denotes that the Process is currently in a single line comment.
+		 */
+		SINGLE_LINE_COMMENT,
+		/**
+		 * Denotes that the process is inside a multi line comment or a javadoc.
+		 */
+		MULTI_LINE_COMMENT
+	}
 
-    /**
-     * Aims to indent the given String using the pattern provided. Will return the String unchanged if {@link IndentationHelper.IndentationType#NULL} is passed as the IndentationType parameter.
-     * @param text The text that should be indented.
-     * @param type The type of indentation to be used.
-     * @return The indented String with the format specified.
-     */
-    public static String formatIndentation(String text, IndentationType type) {
-        if(type == IndentationType.NULL) {
-            return text;
-        }
-        int numberOfBrackets = 0;
-        StringBuilder builder = new StringBuilder((int) (text.length()*1.25f));
-        boolean startOfLine = true;
-        boolean inString = false;
-        boolean inChar = false;
-        boolean singleLineComment = false;
-        boolean multiLineComment = false;
-        for(int i = 0; i< text.length(); i++) {
-            char current = text.charAt(i);
-            switch (current) {
-                case '{' -> {
-                    if(!inString && !singleLineComment && !multiLineComment && !inChar) {
-                        numberOfBrackets++;
-                    }
-                    startOfLine = false;
-                    builder.append(current);
-                }
-                case '}' -> {
-                    if(!inString && !singleLineComment && !multiLineComment && !inChar) {
-                        numberOfBrackets--;
-                    }
-                    if(startOfLine && builder.length()-type.getNumberOfChars() > 0 && !multiLineComment) {
-                        startOfLine = false;
-                        builder.replace(builder.length()-type.getNumberOfChars(),builder.length(),"}");
-                    } else {
-                        startOfLine = false;
-                        builder.append(current);
-                    }
-                }
-                case '"'-> {
-                    if(builder.length() > 0) {
-                        if (isEscaped(builder,builder.length()) || inChar || singleLineComment ||multiLineComment) {
-                            builder.append(current);
-                            break;
-                        }
-                    }
-                    inString = !inString;
-                    builder.append(current);
-                }
-                case '\'' -> {
-                    if(builder.length() > 0) {
-                        if (isEscaped(builder,builder.length()) || inString || singleLineComment ||multiLineComment) {
-                            builder.append(current);
-                            break;
-                        }
-                    }
-                    inChar = !inChar;
-                    builder.append(current);
-                }
-                case '/' -> {
-                    builder.append(current);
-                    startOfLine = false;
-                    if(i+1 < text.length() && !inString && !inChar) {
-                        char nextChar = text.charAt(i+1);
-                        if(nextChar == '/'){
-                            singleLineComment = true;
-                        }
-                        if(nextChar == '*') {
-                            multiLineComment = true;
-                        }
-                    }
-                }
-                case '\n' ->  {
-                    startOfLine = true;
-                    singleLineComment = false;
-                    builder.append("\n").append(type.getPattern().repeat(Math.max(numberOfBrackets,0)));
-                }
-                case '*' -> {
-                    builder.append(current);
-                    startOfLine = false;
-                    if(i+1 < text.length() && !inString && !inChar) {
-                        char nextChar = text.charAt(i+1);
-                        if(nextChar == '/'){
-                            multiLineComment = false;
-                        }
-                    }
-                }
-                default ->  {
-                    if(startOfLine && !Character.isWhitespace(current)) {
-                        builder.append(current);
-                        startOfLine = false;
-                    } else if(!startOfLine) {
-                        builder.append(current);
-                    }
-                }
-            }
-        }
-        return builder.toString();
-    }
 
-    /**
-     * Determines if the character in the StringBuilder at the specified position is escaped.
-     * @param builder the StringBuilder which holds the current String
-     * @param index The index at which the character to be checked is located.
-     * @return True, if the escape character is referring to the character at the index, false otherwise.
-     */
-    private static boolean isEscaped(StringBuilder builder,int index) {
-        int numberOfCharacters = 0;
-        index--;
-        if(index >= builder.length()) {
-            return false;
-        }
-        while(index > 0 && builder.charAt(index) == '\\') {
-            numberOfCharacters++;
-            index--;
-        }
-        return numberOfCharacters % 2 == 1;
-    }
+	/**
+	 * Aims to indent the given String using the pattern provided. Will return the String unchanged if {@link IndentationHelper.IndentationType#NULL} is passed as the IndentationType parameter.
+	 *
+	 * @param text The text that should be indented.
+	 * @param type The type of indentation to be used.
+	 * @return The indented String with the format specified.
+	 */
+	public static String formatIndentation(String text, IndentationType type) {
+		if (type == IndentationType.NULL) {
+			return text;
+		}
+		int numberOfBrackets = 0;
+		StringBuilder builder = new StringBuilder((int) (text.length() * 1.25f));
+		IndentationState currentState = IndentationState.CODE;
+		boolean startOfLine = true;
+		for (int i = 0; i < text.length(); i++) {
+			char current = text.charAt(i);
+			builder.append(current);
+			if (current == '\n') {
+				builder.append(type.getPattern().repeat(Math.max(numberOfBrackets, 0)));
+				startOfLine = true;
+			}
+			if (startOfLine && current == ' ') {
+				builder.deleteCharAt(builder.length() - 1);
+			}
+			switch (currentState) {
+				case CODE -> {
+					switch (current) {
+						case '{' -> numberOfBrackets++;
+						case '}' -> {
+							numberOfBrackets--;
+							if (startOfLine && builder.length() - type.getNumberOfChars() - 1 >= 0) {
+								builder.replace(builder.length() - type.getNumberOfChars() - 1, builder.length(), "}");
+							}
+						}
+						case '\'' -> currentState = IndentationState.CHARACTER;
+						case '\"' -> currentState = IndentationState.STRING;
+						case '/' -> {
+							if (i + 1 < text.length()) {
+								if (text.charAt(i + 1) == '/') {
+									currentState = IndentationState.SINGLE_LINE_COMMENT;
+								} else if (text.charAt(i + 1) == '*') {
+									currentState = IndentationState.MULTI_LINE_COMMENT;
+								}
+							}
+						}
+					}
+				}
+				case STRING -> {
+					if (current == '\"') {
+						if (!isEscaped(builder, builder.length() - 1)) {
+							currentState = IndentationState.CODE;
+						}
+					}
+				}
+				case CHARACTER -> {
+					if (current == '\'') {
+						if (!isEscaped(builder, builder.length() - 1)) {
+							currentState = IndentationState.CODE;
+						}
+					}
+				}
+				case SINGLE_LINE_COMMENT -> {
+					if (current == '\n') {
+						currentState = IndentationState.CODE;
+					}
+				}
+				case MULTI_LINE_COMMENT -> {
+					if (current == '*' && i + 1 < text.length()) {
+						if (text.charAt(i + 1) == '/') {
+							currentState = IndentationState.CODE;
+						}
+					}
+				}
+			}
+			if (!Character.isWhitespace(current) && startOfLine) {
+				startOfLine = false;
+			}
+		}
+		return builder.toString();
+	}
+
+	/**
+	 * Determines if the character in the StringBuilder at the specified position is escaped.
+	 *
+	 * @param builder the StringBuilder which holds the current String
+	 * @param index   The index at which the character to be checked is located.
+	 * @return True, if the escape character is referring to the character at the index, false otherwise.
+	 */
+	private static boolean isEscaped(StringBuilder builder, int index) {
+		int numberOfCharacters = 0;
+		index--;
+		if (index >= builder.length()) {
+			return false;
+		}
+		while (index > 0 && builder.charAt(index) == '\\') {
+			numberOfCharacters++;
+			index--;
+		}
+		return numberOfCharacters % 2 == 1;
+	}
 }
--- src/test/java/net/javadiscord/javabot/util/IndentationHelperTest.java
+++ src/test/java/net/javadiscord/javabot/util/IndentationHelperTest.java
@@ -1,4 +1,5 @@
 package net.javadiscord.javabot.util;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,32 +14,28 @@ import static org.junit.jupiter.api.Assertions.fail;
  * Test for the {@link IndentationHelper} class.
  */
 public class IndentationHelperTest {
-    /**
-     * Tests the {@link IndentationHelper#formatIndentation(String, IndentationHelper.IndentationType)} method
-     */
-    @Test
-    public void testFormatIndentation() {
-        String[] unformatted = null;
-        String[] formatted = null;
-        try {
-            unformatted = Files.readString(Path.of(IndentationHelper.class.getResource("/Unformatted Strings.txt").toURI())).split("----");
-            formatted = Files.readString(Path.of(IndentationHelper.class.getResource("/Formatted Strings.txt").toURI())).split("----");
-        } catch (NullPointerException | URISyntaxException e) {
-            fail("Files to run the test not present");
-        } catch (IOException e) {
-            fail("IO Exception occurred");
-        }
+	/**
+	 * Tests the {@link IndentationHelper#formatIndentation(String, IndentationHelper.IndentationType)} method
+	 */
+	@Test
+	public void testFormatIndentation() {
+		String[] unformatted = null;
+		String[] formatted = null;
+		try {
+			unformatted = Files.readString(Path.of(IndentationHelper.class.getResource("/Unformatted Strings.txt").toURI())).split("----");
+			formatted = Files.readString(Path.of(IndentationHelper.class.getResource("/Formatted Strings.txt").toURI())).split("----");
+		} catch (NullPointerException | URISyntaxException e) {
+			fail("Files to run the test not present", e);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        try {
-            for(int i = 0,k=0; i< unformatted.length; i++) {
-                assertEquals(formatted[k++],   IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.FOUR_SPACES),"Method failed to format a text with four spaces correctly");
-                assertEquals(formatted[k++], IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.TWO_SPACES),"Method failed to format a text with two spaces correctly");
-                assertEquals(formatted[k++], IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.TABS), "Method failed to format a text with tabs correctly.");
-                assertEquals(formatted[k++], IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.NULL), "Method returned a String not matching the input");
-            }
-        } catch (Exception e) {
-            fail("Method threw exception",e.getCause());
-        }
+		for (int i = 0, k = 0; i < unformatted.length; i++) {
+			assertEquals(formatted[k++], IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.FOUR_SPACES), "Method failed to format a text with four spaces correctly");
+			assertEquals(formatted[k++], IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.TWO_SPACES), "Method failed to format a text with two spaces correctly");
+			assertEquals(formatted[k++], IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.TABS), "Method failed to format a text with tabs correctly.");
+			assertEquals(formatted[k++], IndentationHelper.formatIndentation(unformatted[i], IndentationHelper.IndentationType.NULL), "Method returned a String not matching the input");
+		}
 
-    }
+	}
 }
