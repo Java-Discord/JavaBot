c60eddd69353025ca700555803de2367c045925f
danthe1st <daniel@wwwmaster.at>
2023-05-01 12:31:22 +0200

    fix QOTW decrement embed

--- src/main/java/net/javadiscord/javabot/systems/qotw/commands/qotw_points/DecrementPointsSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/qotw/commands/qotw_points/DecrementPointsSubcommand.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.javadiscord.javabot.systems.notification.NotificationService;
+import net.javadiscord.javabot.systems.notification.QOTWNotificationService;
 import net.javadiscord.javabot.systems.qotw.QOTWPointsService;
 
 /**
@@ -28,5 +29,10 @@ public class DecrementPointsSubcommand extends ChangePointsSubcommand {
 	protected @NotNull EmbedBuilder createIncrementEmbedBuilder(User user, long points) {
 		return super.createIncrementEmbedBuilder(user, points)
 				.setTitle("QOTW Account decremented");
+	}
+
+	@Override
+	protected void sendUserNotification(@NotNull QOTWNotificationService notificationService) {
+		notificationService.sendAccountDecrementedNotification();
 	}
 }
