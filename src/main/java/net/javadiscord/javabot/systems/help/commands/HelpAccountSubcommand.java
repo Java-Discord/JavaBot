1ff40f6a308a6ea1531d4e7dd8a22a61ef37c163
danthe1st <daniel@wwwmaster.at>
2023-07-08 22:08:28 +0200

    XP plot

--- src/main/java/net/javadiscord/javabot/systems/help/commands/HelpAccountSubcommand.java
+++ src/main/java/net/javadiscord/javabot/systems/help/commands/HelpAccountSubcommand.java
@@ -2,6 +2,7 @@ package net.javadiscord.javabot.systems.help.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
@@ -9,13 +10,29 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction;
+import net.dv8tion.jda.api.utils.FileUpload;
+import net.javadiscord.javabot.data.config.BotConfig;
 import net.javadiscord.javabot.data.h2db.DbActions;
 import net.javadiscord.javabot.systems.help.HelpExperienceService;
+import net.javadiscord.javabot.systems.help.dao.HelpTransactionRepository;
 import net.javadiscord.javabot.systems.help.model.HelpAccount;
+import net.javadiscord.javabot.util.Checks;
 import net.javadiscord.javabot.util.ExceptionLogger;
 import net.javadiscord.javabot.util.Pair;
+import net.javadiscord.javabot.util.Plotter;
 import net.javadiscord.javabot.util.Responses;
 import net.javadiscord.javabot.util.StringUtils;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
 import org.jetbrains.annotations.NotNull;
 import org.springframework.dao.DataAccessException;
 import xyz.dynxsty.dih4jda.interactions.commands.application.SlashCommand;
@@ -27,26 +44,40 @@ import xyz.dynxsty.dih4jda.interactions.commands.application.SlashCommand;
  */
 public class HelpAccountSubcommand extends SlashCommand.Subcommand {
 
+	private final BotConfig botConfig;
 	private final DbActions dbActions;
 	private final HelpExperienceService helpExperienceService;
+	private final HelpTransactionRepository transactionRepository;
 
 	/**
 	 * The constructor of this class, which sets the corresponding {@link SubcommandData}.
 	 *
+	 * @param botConfig             The bot configuration
 	 * @param dbActions             An object responsible for various database actions
 	 * @param helpExperienceService Service object that handles Help Experience Transactions.
+	 * @param transactionRepository DAO for help XP transactions
 	 */
-	public HelpAccountSubcommand(DbActions dbActions, HelpExperienceService helpExperienceService) {
+	public HelpAccountSubcommand(BotConfig botConfig, DbActions dbActions, HelpExperienceService helpExperienceService, HelpTransactionRepository transactionRepository) {
 		this.dbActions = dbActions;
 		this.helpExperienceService = helpExperienceService;
+		this.botConfig = botConfig;
+		this.transactionRepository = transactionRepository;
 		setCommandData(new SubcommandData("account", "Shows an overview of your Help Account.")
 				.addOption(OptionType.USER, "user", "If set, show the Help Account of the specified user instead.", false)
+				.addOption(OptionType.BOOLEAN, "plot", "generate a plot of help XP history", false)
 		);
 	}
 
 	@Override
 	public void execute(@NotNull SlashCommandInteractionEvent event) {
 		User user = event.getOption("user", event::getUser, OptionMapping::getAsUser);
+		boolean plot = event.getOption("plot", false, OptionMapping::getAsBoolean);
+		
+		if (plot && user.getIdLong()!=event.getUser().getIdLong() && !Checks.hasStaffRole(botConfig, event.getMember())) {
+			Responses.error(event, "You can only plot your own help XP history.").queue();
+			return;
+		}
+		
 		long totalThanks = dbActions.count(
 				"SELECT COUNT(id) FROM help_channel_thanks WHERE helper_id = ?",
 				s -> s.setLong(1, user.getIdLong())
@@ -55,13 +86,57 @@ public class HelpAccountSubcommand extends SlashCommand.Subcommand {
 				"SELECT COUNT(id) FROM help_channel_thanks WHERE helper_id = ? AND thanked_at > DATEADD('week', -1, CURRENT_TIMESTAMP(0))",
 				s -> s.setLong(1, user.getIdLong())
 		);
+		
+		event.deferReply().queue();
+		
+		FileUpload upload = null;
+		if (plot) {
+			upload = generatePlot(user);
+		}
+		
 		try {
 			HelpAccount account = helpExperienceService.getOrCreateAccount(user.getIdLong());
-			event.replyEmbeds(buildHelpAccountEmbed(account, user, event.getGuild(), totalThanks, weekThanks)).queue();
+			WebhookMessageCreateAction<Message> reply = event.getHook().sendMessageEmbeds(buildHelpAccountEmbed(account, user, event.getGuild(), totalThanks, weekThanks));
+			if (upload!=null) {
+				reply.addFiles(upload);
+			}
+			reply.queue();
 		} catch (DataAccessException e) {
 			ExceptionLogger.capture(e, getClass().getSimpleName());
 			Responses.error(event, e.getMessage()).queue();
 		}
+	}
+
+	private FileUpload generatePlot(User user) {
+		List<Pair<Pair<Integer,Integer>,Double>> xpData = transactionRepository.getTotalTransactionWeightByMonth(user.getIdLong(), LocalDate.now().withDayOfMonth(1).minusYears(1).atStartOfDay());
+		
+		if (xpData.isEmpty()) {
+			return null;
+		}
+		
+		List<Pair<String, Double>> plotData = new ArrayList<>();
+		
+		int i = 0;
+		for(LocalDate position = LocalDate.now().minusYears(1); position.isBefore(LocalDate.now().plusDays(1)); position=position.plusMonths(1)) {
+			double value = 0.0;
+			if(i<xpData.size()) {
+				Pair<Pair<Integer, Integer>, Double> entry = xpData.get(i);
+				if(entry.first().first() == position.getMonthValue() && entry.first().second() == position.getYear()) {
+					value = Math.round(entry.second()*100)/100.0;
+					i++;
+				}
+			}
+			plotData.add(new Pair<>(position.getMonth() + " " + position.getYear(), value));
+		}
+		
+		BufferedImage plt = new Plotter(plotData).plot();
+		try(ByteArrayOutputStream os = new ByteArrayOutputStream()){
+			ImageIO.write(plt, "png", os);
+			return FileUpload.fromData(os.toByteArray(), "image.png");
+		} catch (IOException e) {
+			ExceptionLogger.capture(e, "Cannot create XP plot");
+		}
+		return null;
 	}
 
 	private @NotNull MessageEmbed buildHelpAccountEmbed(HelpAccount account, @NotNull User user, Guild guild, long totalThanks, long weekThanks) {
