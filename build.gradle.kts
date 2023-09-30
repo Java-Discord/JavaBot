1d567f4c6200e53043a0c71ac41b10686d61ad74
danthe1st <daniel@wwwmaster.at>
2023-09-30 17:30:16 +0200

    update JDA, use new custom status

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
     // DIH4JDA (Command Framework) & JDA
     implementation("com.github.DynxstyGIT:DIH4JDA:120a15ad2e")
-    implementation("net.dv8tion:JDA:5.0.0-beta.12") {
+    implementation("net.dv8tion:JDA:5.0.0-beta.14") {
         exclude(module = "opus-java")
     }
 
