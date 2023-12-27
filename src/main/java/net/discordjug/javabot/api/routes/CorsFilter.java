4008137b4d3c4e5921344c4ed83a5663ab63379a
danthe1st <daniel@wwwmaster.at>
2023-12-27 16:58:50 +0100

    allow CORS on API via filter

---
+++ src/main/java/net/discordjug/javabot/api/routes/CorsFilter.java
@@ -0,0 +1,27 @@
+package net.discordjug.javabot.api.routes;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * This filter enables CORS for all endpoints.
+ */
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET, OPTIONS");
+		filterChain.doFilter(request, response);
+	}
+
+}
