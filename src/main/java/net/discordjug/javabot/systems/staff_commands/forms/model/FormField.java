fdd328e5fb89f140afabd164e2e7f8f2776e9e7e
Defective <def3ctive4@gmail.com>
2025-09-20 11:01:56 +0200

    Form field javadoc

--- src/main/java/net/discordjug/javabot/systems/staff_commands/forms/model/FormField.java
+++ src/main/java/net/discordjug/javabot/systems/staff_commands/forms/model/FormField.java
@@ -5,12 +5,14 @@ import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 
 /**
  * Represents a form field.
+ * Form fields are used to store data about text inputs presented to the user in the form modal.
+ * Each form can have up to 5 fields.
  */
 public record FormField(String label, int max, int min, String placeholder, boolean required,
 		TextInputStyle style, String value, long id) {
 
 	/**
-	 * Create a standalone text input from this field.
+	 * Create a text input from this field.
 	 *
 	 * @param id ID of this text input.
 	 * @return text input ready to use in a modal.
